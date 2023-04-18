33708a3923a4ba4dff91a59a8241cd311bb48dd7
jaycho-46 <jay.46@quarkonix.com>
2023-04-18 17:01:28 +0900

    Setup redux

---
+++ src/store/index.ts
@@ -0,0 +1,16 @@
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import rootReducer from "./reducer";
+
+export const store = configureStore({
+  reducer: rootReducer,
+  devTools: process.env.NODE_ENV !== "production",
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
