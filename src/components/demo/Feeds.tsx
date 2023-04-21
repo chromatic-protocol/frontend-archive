a1dd5c73fd56fa2eb21b62171c3a5a22bdb6c903
austin-builds <austin.31@quarkonix.com>
2023-04-21 17:26:36 +0900

    feat: use `wagmi` hooks for contract call

--- src/components/demo/Feeds.tsx
+++ src/components/demo/Feeds.tsx
@@ -1,3 +1,14 @@
+import { BigNumber } from "ethers";
+import { useContract, useNetwork, useSigner } from "wagmi";
+import App from "../../abis/demo/App.json";
+import { useCallback, useEffect, useReducer, useState } from "react";
+import type { FormEvent } from "react";
+
+/**
+ * FIXME: need `.env.local`
+ */
+const contractAddress = import.meta.env.VITE_DEMO_CONTRACT;
+
 type Feed = {
   id: number;
   title: string;
@@ -20,3 +31,17 @@ type FormAction<T = "title" | "content"> = T extends "title"
   ? { type: T; payload: Pick<FormState, T> }
   : never;
 
+const Feeds = () => {
+  const { chain } = useNetwork();
+  const { data: signer } = useSigner({
+    chainId: chain?.id,
+    suspense: false,
+  });
+  const contract = useContract({
+    address: contractAddress,
+    abi: App.abi,
+    signerOrProvider: signer,
+  });
+};
+
+export default Feeds;
