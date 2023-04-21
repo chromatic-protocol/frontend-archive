d461db5d517d6c72009ec9a71e8ef1ec6a8dd981
austin-builds <austin.31@quarkonix.com>
2023-04-21 17:27:38 +0900

    feat: fetch and create feeds

--- src/components/demo/Feeds.tsx
+++ src/components/demo/Feeds.tsx
@@ -42,6 +42,101 @@ const Feeds = () => {
     abi: App.abi,
     signerOrProvider: signer,
   });
+  const [feeds, setFeeds] = useState<Feed[]>([]);
+  const [formState, formDispatch] = useReducer(
+    (state: FormState, action: FormAction) => {
+      switch (action.type) {
+        case "title": {
+          state = { ...state, title: action.payload.title };
+          break;
+        }
+        case "content": {
+          state = { ...state, content: action.payload.content };
+          break;
+        }
+      }
+      return state;
+    },
+    { title: "", content: "" } satisfies FormState
+  );
+  const fetchFeeds = useCallback(async () => {
+    if (typeof signer === "undefined") {
+      return;
+    }
+    const response: FeedResponse[] = await contract?.["getAllFeed"]();
+    const nextFeeds = response.map((data) => {
+      return {
+        id: data[0].toNumber(),
+        title: data[1],
+        content: data[2],
+        author: data[3],
+        createdAt: data[4].toNumber(),
+      } satisfies Feed;
+    });
+    setFeeds(nextFeeds);
+  }, [contract, signer]);
+
+  useEffect(() => {
+    fetchFeeds();
+  }, [fetchFeeds]);
+
+  const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    await contract?.["createFeed"](formState.title, formState.content);
+  };
+  return (
+    <div className="w-full flex gap-4">
+      <div className="flex flex-col gap-4 w-full max-w-[720px]">
+        {feeds.map((feed) => (
+          <div className="flex flex-col items-start w-full" key={feed.id}>
+            <div className="flex w-full">
+              <h5 className="font-medium text-md">#{feed.id}</h5>
+              <h5 className="font-medium text-md ml-auto">by {feed.author}</h5>
+            </div>
+            <h3 className="font-bold text-2xl">{feed.title}</h3>
+            <p className="text-lg text-start">{feed.content}</p>
+            <h5>{new Date(feed.createdAt * 1000).toLocaleString()}</h5>
+          </div>
+        ))}
+      </div>
+      <form
+        method="post"
+        className="flex flex-col gap-x-4 gap-y-2 items-start w-full"
+        onSubmit={onSubmit}
+      >
+        <label htmlFor="title">Title</label>
+        <input
+          type="text"
+          name="title"
+          id="title"
+          className="border w-full"
+          value={formState.title}
+          onChange={(event) => {
+            formDispatch({
+              type: "title",
+              payload: { title: event.target.value },
+            });
+          }}
+        />
+        <label htmlFor="content">Content</label>
+        <textarea
+          name="content"
+          id="content"
+          className="w-full border"
+          value={formState.content}
+          onChange={(event) => {
+            formDispatch({
+              type: "content",
+              payload: { content: event.target.value },
+            });
+          }}
+        />
+        <button type="submit" className="bg-black text-white px-2 py-0.5">
+          Feed up!
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Feeds;
