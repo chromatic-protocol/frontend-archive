fe61141e8d1e22503048269f30c1314eb4b3fb08
dianne <dia.nn@quarkonix.com>
2023-04-25 15:05:34 +0900

    Create Dropdown

---
+++ src/stories/atom/Dropdown/stories.ts
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Dropdown } from ".";
+
+const meta = {
+  title: "Atom/Dropdown",
+  component: Dropdown,
+  tags: ["autodocs"],
+  // argTypes: {
+  // },
+} satisfies Meta<typeof Dropdown>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    label: "Dropdown",
+  },
+};
+
+export const Large: Story = {
+  args: {
+    size: "lg",
+    label: "Dropdown",
+  },
+};
+
+export const Small: Story = {
+  args: {
+    size: "sm",
+    label: "Dropdown",
+  },
+};
