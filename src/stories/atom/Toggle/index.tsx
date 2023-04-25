0da790b86d6c674ca47e82477921d20704ace518
dianne <dia.nn@quarkonix.com>
2023-04-25 14:13:56 +0900

    Update Toggle

--- src/stories/atom/Toggle/index.tsx
+++ src/stories/atom/Toggle/index.tsx
@@ -2,39 +2,37 @@ import { useState } from "react";
 import { Switch } from "@headlessui/react";
 
 interface ToggleProps {
-  active?: boolean;
-  backgroundColor?: string;
-  size?: "sm" | "md" | "lg";
+  size?: "sm" | "lg";
   label: string;
   onClick?: () => void;
 }
 
-export const Toggle = ({
-  active = false,
-  size = "md",
-  backgroundColor,
-  label,
-  ...props
-}: ToggleProps) => {
+export const Toggle = ({ size = "lg", label, ...props }: ToggleProps) => {
   const [enabled, setEnabled] = useState(false);
 
   return (
-    <div className="">
+    <>
       <Switch
         checked={enabled}
         onChange={setEnabled}
-        className={`${
+        className={`toggle ${
           enabled ? "bg-active dark:bg-black" : "bg-active/60 dark:bg-black/60"
-        }
-          relative inline-flex h-[38px] w-[74px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+        } ${size === "lg" ? "h-[32px] w-[64px]" : "h-[24px] w-[48px]"}
+          relative inline-flex shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-white focus-visible:ring-opacity-75`}
       >
-        <span className="sr-only">Use setting</span>
+        <span className="sr-only">{label}</span>
         <span
           aria-hidden="true"
-          className={`${enabled ? "translate-x-9" : "translate-x-0"}
-            pointer-events-none inline-block h-[34px] w-[34px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
+          className={`${
+            enabled
+              ? size === "lg"
+                ? "translate-x-8"
+                : "translate-x-6"
+              : "translate-x-0"
+          } ${size === "lg" ? "h-[28px] w-[28px]" : "h-[20px] w-[20px]"}
+            pointer-events-none inline-block transform rounded-full bg-white transition duration-200 ease-in-out`}
         />
       </Switch>
-    </div>
+    </>
   );
 };
