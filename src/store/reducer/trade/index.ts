626167a0f4522a33cf0ef674c35001a79221fe4a
austin-builds <austin.31@quarkonix.com>
2023-04-26 18:51:42 +0900

    wip: trade slice reducer

--- src/store/reducer/trade/index.ts
+++ src/store/reducer/trade/index.ts
@@ -1,3 +1,5 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
 import { BigNumber } from "ethers";
 import { bigNumberify } from "../../../utils/number";
 
@@ -29,3 +31,52 @@ const initialState: TradeState = {
   transactionFee: bigNumberify(0),
   priceDistance: bigNumberify(0),
 };
+
+export const tradeSlice = createSlice({
+  name: "trade",
+  initialState,
+  reducers: {
+    onTypeToggle: (state, action: PayloadAction<"LONG" | "SHORT">) => {
+      state.type = action.payload;
+    },
+    onUnitToggle: (state, action: PayloadAction<"CONTRACT" | "COLLATERAL">) => {
+      state.unit = action.payload;
+    },
+    onCollateralChange: (state, action: PayloadAction<number>) => {
+      state.collateral = bigNumberify(action.payload);
+    },
+    onContractQuantityChange: (state, action: PayloadAction<number>) => {
+      state.contractQuantity = bigNumberify(action.payload);
+    },
+    onLeverageChange: (state, action: PayloadAction<number>) => {
+      state.leverage = action.payload;
+    },
+    onTakeProfitRatioChange: (state, action: PayloadAction<number>) => {
+      state.takeProfitRatio = bigNumberify(action.payload);
+    },
+    onStopLossRatioChange: (state, action: PayloadAction<number>) => {
+      state.stopLossRatio = bigNumberify(action.payload);
+    },
+    onPoolsChange: (state, action: PayloadAction<any[]>) => {
+      state.pools = action.payload;
+    },
+    onTransactionFeeChange: (state, action: PayloadAction<number>) => {
+      const parsed = Number(action.payload);
+      if (isNaN(parsed)) {
+        return state;
+      }
+      state.transactionFee = bigNumberify(parsed);
+    },
+    onPriceDistanceChange: (state, action: PayloadAction<number>) => {
+      const parsed = Number(action.payload);
+      if (isNaN(parsed)) {
+        return state;
+      }
+      state.priceDistance = bigNumberify(parsed);
+    },
+  },
+});
+
+export const tradeAction = tradeSlice.actions;
+
+export const tradeReducer = tradeSlice.reducer;
