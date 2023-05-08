bc3c21bfe644898bbde85b0c495c7ce78aee4e81
austin-builds <austin.31@quarkonix.com>
2023-05-08 15:27:26 +0900

    fix: market type for contract responses

--- src/typings/market.ts
+++ src/typings/market.ts
@@ -1,22 +1,8 @@
-import { BigNumber } from "ethers";
-
 export interface Token {
-  name: string;
-  symbol: string;
-  decimals: number;
   address: string;
-  imageUrl: string;
-  balance: BigNumber;
-  maxPrice: BigNumber;
-  minPrice: BigNumber;
-  interestRate: BigNumber;
-  spread: BigNumber;
 }
 
 export interface Market {
-  name: string;
-  imageUrl: string;
-
-  // Settlement token for market
-  tokenAddress: string;
+  address: string;
+  description: string;
 }
