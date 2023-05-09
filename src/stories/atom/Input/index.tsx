9dfe1ff347113472121807d1b5103ca916b4a3b5
dianne <dia.nn@quarkonix.com>
2023-05-09 14:25:24 +0900

    Update Input placeholder, style

--- src/stories/atom/Input/index.tsx
+++ src/stories/atom/Input/index.tsx
@@ -3,18 +3,22 @@ import "./style.css";
 interface InputProps {
   label?: string;
   value?: string;
+  placeholder?: string;
   type?: string;
   size?: "sm" | "base" | "lg";
   style?: "default" | "active";
+  align?: "center" | "left" | "right";
   disabled?: boolean;
   onClick?: () => unknown;
 }
 
 export const Input = ({
   label,
+  placeholder,
   type,
   size = "base",
   style = "default",
+  align = "left",
   ...props
 }: InputProps) => {
   const value = () => props.value;
@@ -22,8 +26,9 @@ export const Input = ({
   return (
     <input
       type={type}
-      className={`input input-${size} input-${style}`}
+      className={`input input-${size} input-${style} text-${align}`}
       value={value()}
+      placeholder={placeholder}
       aria-label={label}
     />
   );
