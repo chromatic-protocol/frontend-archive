2cf6fe9eddede55731716689ba79fe863dafd288
austin-builds <austin.31@quarkonix.com>
2023-05-11 19:05:11 +0900

    feat: add `balance` property

--- src/hooks/useSettlementToken.ts
+++ src/hooks/useSettlementToken.ts
@@ -1,17 +1,24 @@
 import { useSigner } from "wagmi";
 import useSWR from "swr";
 import { useAppDispatch, useAppSelector } from "../store";
-import { USUMMarketFactory, getDeployedContract } from "@quarkonix/usum";
+import {
+  Account__factory,
+  USUMMarketFactory,
+  getDeployedContract,
+} from "@quarkonix/usum";
 import { errorLog } from "../utils/log";
 import { useEffect, useMemo } from "react";
 import { marketAction } from "../store/reducer/market";
 import { isValid } from "../utils/valid";
 import { USDC } from "../configs/token";
 import useLocalStorage from "./useLocalStorage";
+import useUsumAccount from "./useUsumAccount";
+import { Token } from "../typings/market";
 
 export const useSettlementToken = () => {
   const dispatch = useAppDispatch();
   const { data: signer } = useSigner();
+  const [usumAccount] = useUsumAccount();
   const factory = useMemo(() => {
     if (!isValid(signer)) {
       return;
@@ -22,27 +29,37 @@ export const useSettlementToken = () => {
       signer
     ) as USUMMarketFactory;
   }, [signer]);
-  const fetchKey = isValid(factory) ? [factory] : undefined;
+  const account = useMemo(() => {
+    if (!isValid(usumAccount) || !isValid(signer)) {
+      return;
+    }
+    return Account__factory.connect(usumAccount, signer);
+  }, [usumAccount, signer]);
 
+  const fetchKey =
+    isValid(factory) && isValid(account)
+      ? ([factory, account] as const)
+      : undefined;
   const {
     data: tokens,
     error,
     mutate: fetchTokens,
-  } = useSWR(fetchKey, async ([factory]) => {
-    const response = await factory.registeredSettlementTokens();
-    return response.map((address) => {
-      if (address === USDC) {
-        return {
-          address,
-          name: "USDC",
-        };
-      } else {
-        return {
-          address,
-          name: "URT",
-        };
-      }
-    });
+  } = useSWR(fetchKey, async ([factory, account]) => {
+    const addresses = await factory.registeredSettlementTokens();
+    const response = await Promise.allSettled(
+      addresses.map(async (address) => {
+        const balance = await account.balance(address);
+        const name = address === USDC ? "USDC" : "URT";
+        return { name, address, balance } satisfies Token;
+      })
+    );
+    const nextTokens = response
+      .filter(
+        (result): result is PromiseFulfilledResult<Token> =>
+          result.status === "fulfilled"
+      )
+      .map(({ value }) => value);
+    return nextTokens;
   });
 
   useEffect(() => {
--- src/typings/market.ts
+++ src/typings/market.ts
@@ -3,6 +3,7 @@ import { BigNumber } from "ethers";
 export interface Token {
   name: string;
   address: string;
+  balance: BigNumber;
 }
 
 export interface Market {
