744d5fb6af79bb785a846410c7538316bcff8751
dianne <dia.nn@quarkonix.com>
2023-05-11 10:27:44 +0900

    Update MarketSelect - 내부 중첩된 Popover 제거, asset/market 각각 리스트만 보이게 수정

--- src/stories/molecule/MarketSelect/index.tsx
+++ src/stories/molecule/MarketSelect/index.tsx
@@ -1,6 +1,7 @@
-import { Avatar } from "../../atom/Avatar";
 import { Popover } from "@headlessui/react";
+import { Avatar } from "../../atom/Avatar";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import { ChevronRightIcon } from "@heroicons/react/20/solid";
 import "./style.css";
 
 interface MarketSelectProps {
@@ -13,11 +14,11 @@ export const MarketSelect = ({ ...props }: MarketSelectProps) => {
       <Popover>
         <Popover.Button className="flex items-center gap-3 ml-10">
           <div className="flex items-center gap-1 pr-3 border-r">
-            <Avatar size="xs" />
+            <Avatar size="base" />
             <h4>USDC Market</h4>
           </div>
           <div className="flex items-center gap-1">
-            <Avatar size="xs" />
+            <Avatar size="base" />
             <h4>ETH/USD</h4>
           </div>
           <ChevronDownIcon
@@ -26,7 +27,49 @@ export const MarketSelect = ({ ...props }: MarketSelectProps) => {
           />
         </Popover.Button>
         <Popover.Panel className="popover-panel">
-          <Popover.Group className="relative gap-2 pt-4 border-t">
+          <section className="flex w-full gap-12 pt-4 border-t">
+            {/* select - asset */}
+            <article className="flex flex-col">
+              {/* default */}
+              <button className="flex items-center gap-2 px-4 py-2">
+                <Avatar size="base" />
+                <h4>USDC</h4>
+              </button>
+              {/* selected */}
+              <button className="flex items-center gap-2 px-4 py-2 text-white bg-black">
+                <Avatar size="base" />
+                <h4>USDT</h4>
+                <ChevronRightIcon className="w-4" />
+              </button>
+              <button className="flex items-center gap-2 px-4 py-2">
+                <Avatar size="base" />
+                <h4>IMX</h4>
+              </button>
+            </article>
+
+            {/* select - market */}
+            <article className="flex flex-col flex-auto">
+              {/* default */}
+              <button className="flex items-center justify-between gap-4 px-4 py-2">
+                <div className="flex gap-2">
+                  <Avatar size="base" />
+                  <h4>IMX / USD</h4>
+                </div>
+                <p>$1,542.07</p>
+              </button>
+              {/* selected */}
+              {/* 선택된 페어(asset,market)는 따로 selected 표시 필요할지 확인중 */}
+              <button className="flex items-center justify-between gap-4 px-4 py-2 text-white bg-black">
+                <div className="flex gap-2">
+                  <Avatar size="base" />
+                  <h4>ETH / USD</h4>
+                </div>
+                <p>$1,542.07</p>
+              </button>
+            </article>
+          </section>
+
+          {/* <Popover.Group className="relative gap-2 pt-4 border-t">
             <Popover className="inner-popover">
               <Popover.Button className="flex gap-2 w-[128px] px-4 py-2 inner-popover-button">
                 <Avatar size="xs" />
@@ -72,7 +115,7 @@ export const MarketSelect = ({ ...props }: MarketSelectProps) => {
                 </div>
               </Popover.Panel>
             </Popover>
-          </Popover.Group>
+          </Popover.Group> */}
         </Popover.Panel>
       </Popover>
       <div className="flex items-center gap-4 mr-10">
