076b8339710cad6a5b20ba5398ec83d30f85624e
dianne <dia.nn@quarkonix.com>
2023-05-11 10:28:39 +0900

    Add AssetPopover status - logged in/out

--- src/stories/molecule/AssetPopover/stories.ts
+++ src/stories/molecule/AssetPopover/stories.ts
@@ -9,6 +9,13 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  args: {},
+export const LoggedIn: Story = {
+  args: {
+    user: {
+      name: "Jane Doe",
+      contract: "0x7djf300",
+    },
+  },
 };
+
+export const LoggedOut: Story = {};
