7e837e29cbc4375272bbcfffab8205aa12547217
dianne <dia.nn@quarkonix.com>
2023-05-15 16:18:31 +0900

    Create Checkbox

---
+++ src/stories/atom/Checkbox/stories.ts
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Checkbox } from ".";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta = {
+  title: "Atom/Checkbox",
+  component: Checkbox,
+  tags: ["autodocs"],
+  argTypes: {
+    // backgroundColor: { control: "color" },
+  },
+} satisfies Meta<typeof Checkbox>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Default: Story = {
+  args: {
+    label: "Checkbox",
+  },
+};
