93b6d7bbcd0ec13ed9118c752976bcbcbb2f925e
austin-builds <austin.31@quarkonix.com>
2023-05-15 19:02:36 +0900

    feat: shorten address

--- src/utils/address.ts
+++ src/utils/address.ts
@@ -1,3 +1,16 @@
 import { ethers } from "ethers";
+import { isValid } from "./valid";
 
 export const ADDRESS_ZERO = ethers.constants.AddressZero;
+
+export const trimAddress = (
+  address: string,
+  left: number = 5,
+  right: number = 5
+) => {
+  if (!isValid(address)) {
+    return;
+  }
+
+  return address.slice(0, left) + "..." + address.slice(right * -1);
+};
