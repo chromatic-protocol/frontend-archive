149edca5432c045517f0a3b695078fb7202412a3
austin-builds <austin.31@quarkonix.com>
2023-05-16 19:38:41 +0900

    refactor: `useConsoleOnce`

--- src/pages/demo/wallet-popover/index.tsx
+++ src/pages/demo/wallet-popover/index.tsx
@@ -1,22 +1,18 @@
 import { useAccount } from "wagmi";
 import { WalletPopover } from "../../../stories/molecule/WalletPopover";
 import useUsumAccount from "../../../hooks/useUsumAccount";
-import { useEffect } from "react";
-import useConnectOnce from "../../../hooks/useConnectOnce";
 import { copyText } from "../../../utils/clipboard";
 import { tokensMock } from "../../../mock";
 import usePriceFeed from "../../../hooks/usePriceFeed";
+import useConnectOnce from "../../../hooks/useConnectOnce";
 
 const WalletPopoverDemo = () => {
+  useConnectOnce();
   const { address: walletAddress } = useAccount();
   const [usumAddress, fetchAddress, createAddress] = useUsumAccount();
   const connectOnce = useConnectOnce();
   const tokens = tokensMock;
   const [feed] = usePriceFeed();
-
-  useEffect(() => {
-    connectOnce();
-  }, [connectOnce]);
 
   return (
     <>
