cb24a05ca5ea45ba5b02526fb72ebb6bac420bf0
austin-builds <austin.31@quarkonix.com>
2023-05-17 22:48:17 +0900

    refactor: tokens mockup

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -6,13 +6,11 @@ export const tokensMock: Token[] = [
     address: "0x8FB1E3fC51F3b789dED7557E680551d93Ea9d892",
     name: "USDC",
     decimals: 6,
-    balance: BigNumber.from(100).mul(BigNumber.from(10).pow(6)),
   },
   {
     address: "0x509Ee0d083DdF8AC028f2a56731412edD63223B9",
     name: "USDT",
     decimals: 6,
-    balance: BigNumber.from(100).mul(BigNumber.from(10).pow(6)),
   },
 ];
 export const marketsMock: Record<string, Market[]> = {
