59f25fe42d42b5a487125c894f7a0d0fd3d295ba
austin-builds <austin.31@quarkonix.com>
2023-05-17 23:11:17 +0900

    refactor: `WalletPopover` props

--- src/pages/demo/wallet-popover/index.tsx
+++ src/pages/demo/wallet-popover/index.tsx
@@ -1,27 +1,39 @@
-import { useAccount } from "wagmi";
+import { useAccount, useDisconnect } from "wagmi";
 import { WalletPopover } from "../../../stories/molecule/WalletPopover";
 import useUsumAccount from "../../../hooks/useUsumAccount";
 import { copyText } from "../../../utils/clipboard";
-import { tokensMock } from "../../../mock";
 import usePriceFeed from "../../../hooks/usePriceFeed";
 import useConnectOnce from "../../../hooks/useConnectOnce";
+import useLpToken from "../../../hooks/useLpToken";
+import { useSettlementToken } from "../../../hooks/useSettlementToken";
+import useBalances from "../../../hooks/useBalances";
+import { useMarket } from "../../../hooks/useMarket";
 
 const WalletPopoverDemo = () => {
   useConnectOnce();
   const { address: walletAddress } = useAccount();
+  const [tokens] = useSettlementToken();
+  const [markets] = useMarket();
   const [usumAddress, fetchAddress, createAddress] = useUsumAccount();
-  const connectOnce = useConnectOnce();
-  const tokens = tokensMock;
+  const { walletBalances } = useBalances();
+  const [lpTokens] = useLpToken();
+  const { disconnectAsync } = useDisconnect();
   const [feed] = usePriceFeed();
 
   return (
     <>
       <WalletPopover
         account={{ walletAddress, usumAddress }}
-        tokens={tokens ?? []}
+        tokens={tokens}
+        markets={markets}
+        balances={walletBalances}
+        lpTokens={lpTokens}
         priceFeed={feed}
         onUsumCopy={copyText}
         onWalletCopy={copyText}
+        onDisconnect={() => {
+          disconnectAsync?.();
+        }}
       />
     </>
   );
