6d266c5ddf30aad3c655a4d83d37e21adc380fde
jaycho-46 <jay.46@quarkonix.com>
2023-05-22 10:02:30 +0900

    Fix to accept non-serializable value
    Signed-off-by: jaycho-46 <jay.46@quarkonix.com>

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,6 +5,8 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 export const store = configureStore({
   reducer: rootReducer,
   devTools: process.env.NODE_ENV !== "production",
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
 });
 
 export type AppDispatch = typeof store.dispatch;
