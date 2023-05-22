95da74fa06df5b3e2473ff3edecc06cf9f59a088
austin-builds <austin.31@quarkonix.com>
2023-05-22 20:14:41 +0900

    fix: story for `PoolPanel`

--- src/stories/template/PoolPanel/stories.ts
+++ src/stories/template/PoolPanel/stories.ts
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { PoolPanel } from ".";
+import { BigNumber } from "ethers";
 
 const meta = {
   title: "Template/PoolPanel",
@@ -15,9 +16,31 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    user: {
-      name: "Jane Doe",
-      contract: "0x7djf300",
+    market: {
+      address: "0x8888888888888888888888888888888888888888",
+      description: "ETH / USD",
+      price: BigNumber.from(1000),
     },
+    token: {
+      name: "USDC",
+      address: "0x8888888888888888888888888888888888888888",
+      decimals: 6,
+    },
+    balances: {
+      USDC: BigNumber.from(100),
+    },
+    lpToken: {
+      tokenAddress: "0x8888888888888888888888888888888888888888",
+      marketAddress: "0x8888888888888888888888888888888888888888",
+      slots: [],
+    },
+    amount: "1000",
+    indexes: [35, 36],
+    rates: [-0.01, 0.01],
+    bins: 2,
+    longTotalMaxLiquidity: BigNumber.from(100),
+    longTotalUnusedLiquidity: BigNumber.from(100),
+    shortTotalMaxLiquidity: BigNumber.from(100),
+    shortTotalUnusedLiquidity: BigNumber.from(100),
   },
 };
