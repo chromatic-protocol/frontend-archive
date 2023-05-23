6a8e94319cefdfe0bc67ebf5f3b47b77a54bdd3e
dianne <dia.nn@quarkonix.com>
2023-05-23 14:44:11 +0900

    Update TradePanel detail

--- src/stories/template/TradePanel/index.tsx
+++ src/stories/template/TradePanel/index.tsx
@@ -6,26 +6,42 @@ import "../../atom/Tabs/style.css";
 export const TradePanel = () => (
   <div className="inline-flex flex-col mx-auto border">
     {/* TradePanel 확장형 */}
-    <div className="tabs tabs-line tabs-lg">
+    <div className="relative tabs tabs-line tabs-lg">
       <div className="flex">
-        <h2>SHORT</h2>
-        <h2>LONG</h2>
+        <div className="w-[100vw] max-w-[680px] px-0 pb-5 pt-[36px] border-r">
+          <h2 className="border-b-2 border-black max-w-[240px] mx-auto text-2xl font-bold pb-2">
+            SHORT
+          </h2>
+        </div>
+        <div className="w-[100vw] max-w-[680px] px-0 pb-5 pt-[36px]">
+          <h2 className="border-b-2 border-black max-w-[240px] mx-auto text-2xl font-bold pb-2">
+            LONG
+          </h2>
+        </div>
       </div>
       <div className="flex">
-        <div className="w-[100vw] max-w-[680px] px-0 pb-10 pt-[36px]">
+        <div className="w-[100vw] max-w-[680px] px-0 pb-10 pt-[36px] border-r">
           <TradeContent />
         </div>
         <div className="w-[100vw] max-w-[680px] px-0 pb-10 pt-[36px]">
           <TradeContent />
         </div>
+      </div>
+      <div>
+        <Button label=">>" className="absolute left-[-24px] top-10" />
+        <Button label="<<" className="absolute right-[-24px] top-10" />
       </div>
     </div>
     {/* TradePanel 축소형 (tab 있음) */}
-    {/* <div className="tabs tabs-line tabs-lg">
+    {/* <div className="relative tabs tabs-line tabs-lg">
       <Tab.Group>
         <Tab.List className="w-[100vw] max-w-[680px] mx-auto px-10 pt-[36px] flex gap-10">
-          <Tab>SHORT</Tab>
-          <Tab>LONG</Tab>
+          <Tab className="border-b-2 border-black max-w-[240px] mx-auto text-2xl font-bold pb-2">
+            SHORT
+          </Tab>
+          <Tab className="border-b-2 border-black max-w-[240px] mx-auto text-2xl font-bold pb-2">
+            LONG
+          </Tab>
         </Tab.List>
         <Tab.Panels className="flex flex-col items-center w-full">
           <Tab.Panel className="w-[100vw] max-w-[680px] px-0 pb-10 pt-[36px]">
@@ -36,6 +52,10 @@ export const TradePanel = () => (
           </Tab.Panel>
         </Tab.Panels>
       </Tab.Group>
+      <div>
+        <Button label="<<" className="absolute left-[-24px] top-10" />
+        <Button label=">>" className="absolute right-[-24px] top-10" />
+      </div>
     </div> */}
   </div>
 );
