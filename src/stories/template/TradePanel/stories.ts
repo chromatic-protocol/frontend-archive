328113689ab19a76f6b5c18ffa4737fdb3cc6acd
austin-builds <austin.31@quarkonix.com>
2023-05-25 17:43:01 +0900

    feat: story of `TradePanel`

--- src/stories/template/TradePanel/stories.ts
+++ src/stories/template/TradePanel/stories.ts
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { TradePanel } from ".";
+import { BigNumber } from "ethers";
 
 const meta = {
   title: "Template/TradePanel",
@@ -15,9 +16,37 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    user: {
-      name: "Jane Doe",
-      contract: "0x7djf300",
+    longInput: {
+      method: "collateral",
+      quantity: 0,
+      collateral: 0,
+      takeProfit: 10,
+      stopLoss: 100,
+      takerMargin: 0,
+      makerMargin: 0,
+      leverage: 1,
     },
+    shortInput: {
+      method: "collateral",
+      quantity: 0,
+      collateral: 0,
+      takeProfit: 10,
+      stopLoss: 100,
+      takerMargin: 0,
+      makerMargin: 0,
+      leverage: 1,
+    },
+    balances: {
+      USDC: BigNumber.from(1000),
+    },
+    token: {
+      name: "USDC",
+      address: "0x8888888888888888888888888888888888888888",
+      decimals: 6,
+    },
+    longTotalMaxLiquidity: BigNumber.from(10000),
+    longTotalUnusedLiquidity: BigNumber.from(10000),
+    shortTotalMaxLiquidity: BigNumber.from(10000),
+    shortTotalUnusedLiquidity: BigNumber.from(10000),
   },
 };
