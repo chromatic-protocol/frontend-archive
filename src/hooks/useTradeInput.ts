dfd9606d97c667bf6f471f730acc11b7d49b3aee
austin-builds <austin.31@quarkonix.com>
2023-05-25 16:57:21 +0900

    feat: reducer for trade input values

--- src/hooks/useTradeInput.ts
+++ src/hooks/useTradeInput.ts
@@ -1,65 +1,150 @@
-import { useState } from "react";
-import { useAppDispatch, useAppSelector } from "../store";
-import { tradeAction } from "../store/reducer/trade";
+import { ChangeEvent, useReducer } from "react";
+import { useSelectedToken } from "./useSettlementToken";
+import { useSelectedMarket } from "./useMarket";
+import { errorLog } from "../utils/log";
+import { USUMRouter, getDeployedContract } from "@quarkonix/usum";
+import { CHAIN } from "~/constants/contracts";
+import { useSigner } from "wagmi";
+import { isValid } from "~/utils/valid";
+import { TradeInput, TradeInputAction } from "~/typings/trade";
+import { trimLeftZero } from "~/utils/number";
 
-const useTradeInput = () => {
-  const { type, unit, pools } = useAppSelector((state) => state.trade);
-  const dispatch = useAppDispatch();
+const tradeInputInitial = {
+  method: "collateral",
+  quantity: 0,
+  collateral: 0,
+  takeProfit: 10,
+  stopLoss: 100,
+  takerMargin: 0,
+  makerMargin: 0,
+  leverage: 1,
+} satisfies TradeInput;
 
-  const [leverage, setLeverage] = useState("");
-  const [collateral, setCollateral] = useState("");
-  const [contractQuantity, setContractQuantity] = useState("");
-  const [takeProfitRate, setTakeProfitRate] = useState("");
-  const [stopLossRate, setStopLossRate] = useState("");
-
-  const onTypeToggle = (nextType: typeof type) => {
-    dispatch(tradeAction.onTypeToggle(nextType));
+const tradeInputReducer = (state: TradeInput, action: TradeInputAction) => {
+  if (action.type === "method") {
+    const nextMethod =
+      state.method === "collateral" ? "quantity" : "collateral";
+    switch (nextMethod) {
+      case "collateral": {
+        const { collateral, leverage, takeProfit } = state;
+        state = {
+          method: nextMethod,
+          collateral,
+          quantity: collateral * leverage,
+          leverage,
+          takeProfit,
+          stopLoss: 100 / leverage,
+          takerMargin: collateral,
+          makerMargin: collateral * (takeProfit / 100) * leverage,
   };
-
-  const onUnitToggle = (nextUnit: typeof unit) => {
-    dispatch(tradeAction.onUnitToggle(nextUnit));
+        break;
+      }
+      case "quantity": {
+        const { quantity, leverage, takeProfit, stopLoss } = state;
+        state = {
+          method: nextMethod,
+          quantity,
+          collateral: quantity * (stopLoss / 100),
+          takeProfit,
+          stopLoss,
+          leverage,
+          takerMargin: quantity * (stopLoss / 100),
+          makerMargin: quantity * (takeProfit / 100),
   };
+        break;
+      }
+    }
 
-  const onLeverageChange = (nextValue: string) => {
-    if (nextValue.length === 0) {
-      setLeverage("");
-      dispatch(tradeAction.onLeverageChange(0));
-      return;
+    return state;
     }
-    const parsed = Number(nextValue);
-    if (isNaN(parsed)) {
-      return;
+  const { type, payload } = action;
+  const { method } = state;
+
+  switch (type) {
+    case "collateral": {
+      const { collateral } = payload;
+      state = {
+        ...state,
+        collateral,
+        quantity: collateral * state.leverage,
+        takerMargin: collateral,
+        makerMargin: collateral * state.leverage * (state.takeProfit / 100),
+      };
+      break;
     }
-    setLeverage(nextValue);
-    dispatch(tradeAction.onLeverageChange(parsed));
+    case "quantity": {
+      const { quantity } = payload;
+      state = {
+        ...state,
+        quantity,
+        collateral: quantity * (state.stopLoss / 100),
+        takerMargin: quantity * (state.stopLoss / 100),
+        makerMargin: quantity * (state.takeProfit / 100),
   };
-
-  const onCollateralChange = (nextValue: string) => {
-    if (nextValue.length === 0) {
-      setCollateral("");
-      dispatch(tradeAction.onCollateralChange(0));
-      return;
+      break;
     }
-    const parsed = Number(nextValue);
-    if (isNaN(parsed)) {
-      return;
+    case "takeProfit": {
+      const { takeProfit } = payload;
+      if (method === "collateral") {
+        state = {
+          ...state,
+          takeProfit,
+          makerMargin: state.collateral * state.leverage * (takeProfit / 100),
+        };
+      } else {
+        state = {
+          ...state,
+          takeProfit,
+          makerMargin: Number(state.quantity) * (takeProfit / 100),
+        };
     }
-    setCollateral(nextValue);
-    dispatch(tradeAction.onCollateralChange(parsed));
+      break;
+    }
+    case "stopLoss": {
+      const { stopLoss } = payload;
+      if (method === "collateral") {
+        const nextStopRoss = stopLoss === 0 ? 1 : stopLoss;
+        state = {
+          ...state,
+          stopLoss: nextStopRoss,
+          leverage: 100 / nextStopRoss,
+          quantity: state.collateral * (100 / nextStopRoss),
+          makerMargin:
+            state.collateral * (100 / nextStopRoss) * (state.takeProfit / 100),
+        };
+      } else {
+        state = {
+          ...state,
+          stopLoss,
+          collateral: Number(state.quantity) * (stopLoss / 100),
+          takerMargin: Number(state.quantity) * (stopLoss / 100),
+        };
+      }
+      break;
+    }
+    case "leverage": {
+      const { leverage } = payload;
+      if (method === "collateral") {
+        const nextLeverage = leverage === 0 ? 1 : leverage;
+        state = {
+          ...state,
+          leverage: nextLeverage,
+          quantity: state.collateral * nextLeverage,
+          stopLoss: 100 / nextLeverage,
+          makerMargin:
+            state.collateral * nextLeverage * (state.takeProfit / 100),
+        };
+      } else {
+        state = {
+          ...state,
+          leverage,
   };
-
-  const onContractQuantityChange = (nextValue: string) => {
-    if (nextValue.length === 0) {
-      setContractQuantity("");
-      dispatch(tradeAction.onContractQuantityChange(0));
-      return;
     }
-    const parsed = Number(nextValue);
-    if (isNaN(parsed)) {
-      return;
+      break;
     }
-    setContractQuantity(nextValue);
-    dispatch(tradeAction.onContractQuantityChange(parsed));
+  }
+  return state;
+};
   };
 
   const onTakeProfitRateChange = (nextValue: string) => {
