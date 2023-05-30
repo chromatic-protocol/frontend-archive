8fbe78ec05a93e663bc7000ff31d2cf7e5dcb208
austin-builds <austin.31@quarkonix.com>
2023-05-30 17:11:18 +0900

    refactor: unused reducers

--- src/store/reducer/index.ts
+++ src/store/reducer/index.ts
@@ -1,9 +1,7 @@
 import { marketReducer } from "./market";
 import { poolsReducer } from "./pools";
-import { tradeReducer } from "./trade";
 
 const rootReducer = {
-  trade: tradeReducer,
   market: marketReducer,
   pools: poolsReducer,
 };
