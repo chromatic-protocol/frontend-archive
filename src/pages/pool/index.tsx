e61dd6ccbe0133d31cf21b73cf64626054f63b9a
austin-builds <austin.31@quarkonix.com>
2023-06-01 18:47:03 +0900

    feat: state binding in the page `pool`

--- src/pages/pool/index.tsx
+++ src/pages/pool/index.tsx
@@ -6,25 +6,118 @@ import { Footer } from "../../stories/template/Footer";
 import { Button } from "../../stories/atom/Button";
 import { Square2StackIcon } from "@heroicons/react/24/outline";
 import "./style.css";
-
-type User = {
-  name: string;
-};
+import { useAccount, useConnect, useDisconnect } from "wagmi";
+import useConnectOnce from "~/hooks/useConnectOnce";
+import { useUsumAccount } from "~/hooks/useUsumAccount";
+import {
+  useSelectedToken,
+  useSettlementToken,
+} from "~/hooks/useSettlementToken";
+import { useMarket, useSelectedMarket } from "~/hooks/useMarket";
+import { useUsumBalances, useWalletBalances } from "~/hooks/useBalances";
+import usePriceFeed from "~/hooks/usePriceFeed";
+import {
+  useLiquidityPoolSummary,
+  useSelectedLiquidityPool,
+} from "~/hooks/useLiquidityPool";
+import useTokenTransaction from "~/hooks/useTokenTransaction";
+import { copyText } from "~/utils/clipboard";
+import { useFeeRate } from "~/hooks/useFeeRate";
+import { Link } from "react-router-dom";
+import { trimAddress } from "~/utils/address";
+import usePoolInput from "~/hooks/usePoolInput";
 
 const Pool = () => {
-  const [user, setUser] = React.useState<User>();
+  useConnectOnce();
+  const { connectAsync } = useConnect();
+  const { address: walletAddress } = useAccount();
+  const [usumAccount, createUsumAccount] = useUsumAccount();
+  const [tokens] = useSettlementToken();
+  const [markets] = useMarket();
+  const [selectedToken, onTokenSelect] = useSelectedToken();
+  const [selectedMarket, onMarketSelect] = useSelectedMarket();
+  const feeRate = useFeeRate();
+  const [walletBalances] = useWalletBalances();
+  const [usumBalances] = useUsumBalances();
+  const [priceFeed] = usePriceFeed();
+  const pools = useLiquidityPoolSummary();
+  const { disconnectAsync } = useDisconnect();
+  const [balanceAmount, onBalanceAmountChange, onDeposit, onWithdraw] =
+    useTokenTransaction();
+  const [
+    pool,
+    [
+      longTotalMaxLiquidity,
+      longTotalUnusedLiquidity,
+      shortTotalMaxLiquidity,
+      shortTotalUnusedLiquidity,
+    ],
+  ] = useSelectedLiquidityPool();
+  const {
+    amount,
+    indexes,
+    rates,
+    bins,
+    onAmountChange,
+    onRangeChange,
+    onFullRangeSelect,
+    onAddLiquidity,
+  } = usePoolInput();
 
   return (
     <div className="flex flex-col min-h-[100vh] w-full">
       <Header
-        // user={user}
-        onLogin={() => setUser({ name: "Jane Doe" })}
-        onLogout={() => setUser(undefined)}
-        onCreateAccount={() => setUser({ name: "Jane Doe" })}
+        account={{ walletAddress, usumAddress: usumAccount?.address }}
+        tokens={tokens}
+        markets={markets}
+        priceFeed={priceFeed}
+        balances={walletBalances}
+        pools={pools}
+        onConnect={connectAsync}
+        onCreateAccount={createUsumAccount}
+        onDisconnect={disconnectAsync}
+        onWalletCopy={copyText}
+        onUsumCopy={copyText}
       />
       <section className="flex flex-col grow">
-        <MainBar />
-        <PoolPanel />
+        <MainBar
+          account={{ walletAddress, usumAddress: usumAccount?.address }}
+          tokens={tokens}
+          markets={markets}
+          selectedToken={selectedToken}
+          selectedMarket={selectedMarket}
+          feeRate={feeRate}
+          walletBalances={walletBalances}
+          usumBalances={usumBalances}
+          amount={balanceAmount}
+          onTokenSelect={(token) => {
+            onTokenSelect(token);
+          }}
+          onMarketSelect={(market) => {
+            onMarketSelect(market);
+          }}
+          onAmountChange={onBalanceAmountChange}
+          onDeposit={onDeposit}
+          onWithdraw={onWithdraw}
+          onConnect={connectAsync}
+        />
+        <PoolPanel
+          token={selectedToken}
+          balances={walletBalances}
+          pool={pool}
+          amount={amount}
+          indexes={indexes}
+          rates={rates}
+          bins={bins}
+          longTotalMaxLiquidity={longTotalMaxLiquidity}
+          longTotalUnusedLiquidity={longTotalUnusedLiquidity}
+          shortTotalMaxLiquidity={shortTotalMaxLiquidity}
+          shortTotalUnusedLiquidity={shortTotalUnusedLiquidity}
+          onAmountChange={onAmountChange}
+          onRangeChange={onRangeChange}
+          onFullRangeSelect={onFullRangeSelect}
+          onAddLiquidity={onAddLiquidity}
+        />
         <article className="max-w-[680px] w-full mt-8 mx-auto">
           <div className="flex items-center justify-between">
             <div className="flex items-center gap-1">
@@ -33,7 +126,9 @@ const Pool = () => {
             </div>
             <div className="flex items-center gap-1">
               {/* address */}
-              <p>0xdac1...432ec1</p>
+              {selectedToken && (
+                <p>{trimAddress(selectedToken.address, 6, 6)}</p>
+              )}
               <Button iconOnly={<Square2StackIcon />} />
             </div>
           </div>
@@ -43,7 +138,9 @@ const Pool = () => {
               Borrow Fee. Calculated based on open Interest and stop profit/Loss
               rate.
             </p>
-            <Button label="Trade on this ETH/USDC Pool" />
+            <Link to={"/trade"}>
+              <Button label="Trade on this ETH/USDC Pool" />
+            </Link>
           </div>
         </article>
         {/*   info bottom */}
