477701343758ce84d74f462040dfea509b51d1ca
dianne <dia.nn@quarkonix.com>
2023-06-01 17:13:21 +0900

    style: Update RemoveLiquidityModal * Button, ModalCloseButton style * OptionInput style, button shadow * Modal style, Close button function

--- src/stories/atom/ModalCloseButton/stories.ts
+++ src/stories/atom/ModalCloseButton/stories.ts
@@ -1,15 +1,15 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { DialogCloseButton } from ".";
+import { ModalCloseButton } from ".";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta = {
-  title: "Atom/DialogCloseButton",
-  component: DialogCloseButton,
+  title: "Atom/ModalCloseButton",
+  component: ModalCloseButton,
   tags: ["autodocs"],
   argTypes: {
     // backgroundColor: { control: "color" },
   },
-} satisfies Meta<typeof DialogCloseButton>;
+} satisfies Meta<typeof ModalCloseButton>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
