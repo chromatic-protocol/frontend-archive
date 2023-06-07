ba441c3abd984c8cd4b6f52dabed93973ad178fc
austin-builds <austin.31@quarkonix.com>
2023-06-07 17:45:17 +0900

    feat: render positions

--- src/stories/template/TradeBar/index.tsx
+++ src/stories/template/TradeBar/index.tsx
@@ -6,16 +6,14 @@ import { ChevronDoubleDownIcon } from "@heroicons/react/20/solid";
 import { Tab } from "@headlessui/react";
 import { Listbox } from "@headlessui/react";
 import "../../atom/Tabs/style.css";
-
-type User = {
-  name: string;
-  contract: string;
-};
+import { Position } from "~/typings/position";
+import { Market, Token } from "~/typings/market";
+import { usePrevious } from "~/hooks/usePrevious";
 
 interface TradeBarProps {
-  user?: User;
-  // onLogin: () => void;
-  // onLogout: () => void;
+  token?: Token;
+  markets?: Market[];
+  positions?: Position[];
 }
 
 const listitem = [
@@ -23,7 +21,8 @@ const listitem = [
   { id: 2, title: "Positions only in ETH/USD market" },
 ];
 
-export const TradeBar = ({ user }: TradeBarProps) => {
+export const TradeBar = ({ token, markets, positions }: TradeBarProps) => {
+  const previousPositions = usePrevious(positions);
   const [selectedItem, setSelectedItem] = useState(listitem[0]);
 
   return (
@@ -68,70 +67,82 @@ export const TradeBar = ({ user }: TradeBarProps) => {
               <div className="flex flex-col gap-3">
                 {/* 리스트 한개 단위: 리스트 + entry time */}
                 <div>
-                  <div className="flex items-center justify-between gap-2 py-2 text-center border px-7">
-                    <div className="w-[4%]">
-                      {/* Asset */}
-                      <Avatar
-                        label="USDC"
-                        size="xs"
-                        gap="1"
-                        fontSize="base"
-                        fontWeight="bold"
-                      />
+                  {(positions ?? previousPositions ?? []).map((position) => (
+                    <div
+                      key={position.id.toString()}
+                      className="flex items-center justify-between gap-2 py-2 text-center border px-7"
+                    >
+                      <div className="w-[4%]">
+                        {/* Asset */}
+                        <Avatar
+                          label={token?.name}
+                          size="xs"
+                          gap="1"
+                          fontSize="base"
+                          fontWeight="bold"
+                        />
+                      </div>
+                      <div className="w-[8%]">
+                        {/* Market */}
+                        <Avatar
+                          label={
+                            markets?.find(
+                              (market) =>
+                                market.address === position.marketAddress
+                            )?.description
+                          }
+                          size="xs"
+                          gap="1"
+                          fontSize="base"
+                          fontWeight="bold"
+                        />
+                      </div>
+                      <div className="w-[4%]">
+                        {/* Posiiton */}
+                        <Tag label="long" />
+                        {/* <Tag label="long" /> */}
+                      </div>
+                      <div className="w-[6%]">
+                        {/* Entry Price */}${position.renderOpenPrice(18)}
+                      </div>
+                      <div className="w-[6%]">
+                        {/* Contract Qty */}
+                        {position.renderQty(token?.decimals)}
+                      </div>
+                      <div className="w-[4%]">
+                        {/* Collateral */}
+                        {position.renderCollateral(token?.decimals)}
+                      </div>
+                      <div className="w-[6%]">
+                        {/* Take Profit */}
+                        {position.takeProfit}%
+                      </div>
+                      <div className="w-[6%]">
+                        {/* TP Liq. Price */}
+                        <p>{position.renderProfitPrice(18)}</p>
+                        <p className="text-sm text-black/30">
+                          {position.renderToProfit(18)}
+                        </p>
+                      </div>
+                      <div className="w-[4%]">
+                        {/* Stop Loss */}
+                        {position.stopLoss}%
+                      </div>
+                      <div className="w-[6%]">
+                        {/* SL Liq. Price */}
+                        <p>{position.renderLossPrice(18)}</p>
+                        <p className="text-sm text-black/30">
+                          {position.renderToLoss(18)}
+                        </p>
+                      </div>
+                      <div className="w-[4%]">
+                        {/* PnL */}+{position.renderPNL(18)}
+                      </div>
+                      <div className="w-[4%] text-right">
+                        <Button label="Close" />
+                      </div>
                     </div>
-                    <div className="w-[8%]">
-                      {/* Market */}
-                      <Avatar
-                        label="ETH / USD"
-                        size="xs"
-                        gap="1"
-                        fontSize="base"
-                        fontWeight="bold"
-                      />
-                    </div>
-                    <div className="w-[4%]">
-                      {/* Posiiton */}
-                      <Tag label="short" />
-                      {/* <Tag label="long" /> */}
-                    </div>
-                    <div className="w-[6%]">
-                      {/* Entry Price */}
-                      $1,743.50
-                    </div>
-                    <div className="w-[6%]">
-                      {/* Contract Qty */}
-                      2,000
-                    </div>
-                    <div className="w-[4%]">
-                      {/* Collateral */}
-                      2,000
-                    </div>
-                    <div className="w-[6%]">
-                      {/* Take Profit */}
-                      10%
-                    </div>
-                    <div className="w-[6%]">
-                      {/* TP Liq. Price */}
-                      <p>$1,743.50</p>
-                      <p className="text-sm text-black/30">+0.44% higher</p>
-                    </div>
-                    <div className="w-[4%]">
-                      {/* Stop Loss */}
-                      10%
-                    </div>
-                    <div className="w-[6%]">
-                      {/* SL Liq. Price */}
-                      <p>$1,743.50</p>
-                      <p className="text-sm text-black/30">+0.44% higher</p>
-                    </div>
-                    <div className="w-[4%]">
-                      {/* PnL */}
-                      +14.23%
-                    </div>
-                    <div className="w-[4%] text-right">
-                      <Button label="Close" />
-                    </div>
-                  </div>
+                  ))}
                   <p className="text-right text-sm text-[#CBCBCB] mt-2">
                     {/* entry time */}
                     Entry time: October 28, 2022
