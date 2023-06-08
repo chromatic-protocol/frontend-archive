30f0ac651861edb8cbca6a9e4682db932045bb73
austin-builds <austin.31@quarkonix.com>
2023-06-08 17:24:06 +0900

    fix: save previous data to show

--- src/hooks/usePrevious.ts
+++ src/hooks/usePrevious.ts
@@ -1,11 +1,18 @@
 import { useEffect, useRef } from "react";
+import { isValid } from "~/utils/valid";
 
-export const usePrevious = <T>(newValue: T) => {
+export const usePrevious = <T>(newValue: T, save?: boolean) => {
   const ref = useRef<T>();
 
   useEffect(() => {
-    ref.current = newValue;
-  }, [newValue]);
+    if (save) {
+      if (isValid(newValue)) {
+        ref.current = newValue;
+      }
+    } else {
+      ref.current = newValue;
+    }
+  }, [newValue, save]);
 
   return ref.current;
 };
