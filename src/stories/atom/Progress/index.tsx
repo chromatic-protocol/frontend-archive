a4f2c7ba50bd2bec881b5fe5758ba440776b8d79
dianne <dia.nn@quarkonix.com>
2023-06-08 14:16:16 +0900

    style: Add css prop in Progress

--- src/stories/atom/Progress/index.tsx
+++ src/stories/atom/Progress/index.tsx
@@ -1,19 +1,22 @@
 import PropTypes from "prop-types";
+import "./style.css";
 
 interface ProgressProps {
   value?: number;
   max?: number;
+  css?: "default" | "sm";
 }
 
 export const Progress = (props: ProgressProps) => {
-  const { value, max } = props;
+  const { value, max, css = "default" } = props;
   const progressPercentage = (value / max) * 100;
 
   return (
-    <div className="w-full h-1 overflow-hidden rounded bg-gray">
+    <div
+      className={`progress progress-${css} w-full h-1 overflow-hidden rounded bg-gray`}
+    >
       <div
-        className="h-full bg-black rounded"
-        style={{ width: `${progressPercentage}%` }}
+        className={`h-full bg-black rounded w-[${progressPercentage}%]`}
       ></div>
     </div>
   );
