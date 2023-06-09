9f6751261e420ed811dd0855dd0fb5fc7eb90da1
dianne <dia.nn@quarkonix.com>
2023-06-09 18:07:03 +0900

    new: Create TextRow

---
+++ src/stories/atom/TextRow/stories.ts
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { TextRow } from ".";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta = {
+  title: "Atom/TextRow",
+  component: TextRow,
+  tags: ["autodocs"],
+  argTypes: {
+    // backgroundColor: { control: "color" },
+  },
+} satisfies Meta<typeof TextRow>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Default: Story = {
+  args: {
+    label: "label",
+    value: "value",
+  },
+};
