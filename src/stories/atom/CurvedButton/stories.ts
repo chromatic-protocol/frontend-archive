29e724a5d6a67c57be7a81003a4400efb274075c
--global <--global>
2023-06-14 17:33:06 +0900

    new: Create CurvedButton

---
+++ src/stories/atom/CurvedButton/stories.ts
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { CurvedButton } from ".";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta = {
+  title: "Atom/CurvedButton",
+  component: CurvedButton,
+  tags: ["autodocs"],
+  argTypes: {
+    // backgroundColor: { control: "color" },
+  },
+} satisfies Meta<typeof CurvedButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Left: Story = {
+  args: {
+    direction: "left",
+  },
+};
+
+export const Right: Story = {
+  args: {
+    direction: "right",
+  },
+};
