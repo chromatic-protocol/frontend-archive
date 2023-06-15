0de88e1256b2359be2ff5d44ac5837124724c086
--global <--global>
2023-06-15 10:10:19 +0900

    style: Update pool bottom

--- src/pages/pool/index.tsx
+++ src/pages/pool/index.tsx
@@ -5,7 +5,9 @@ import { PoolPanel } from "../../stories/template/PoolPanel";
 import { PoolProgress } from "~/stories/molecule/PoolProgress";
 import { Footer } from "../../stories/template/Footer";
 import { Button } from "../../stories/atom/Button";
+import { AddressCopyButton } from "~/stories/atom/AddressCopyButton";
 import { Square2StackIcon } from "@heroicons/react/24/outline";
+import { ChevronRightIcon } from "@heroicons/react/24/outline";
 import "./style.css";
 import { useAccount, useConnect, useDisconnect } from "wagmi";
 import useConnectOnce from "~/hooks/useConnectOnce";
@@ -158,9 +160,18 @@ const Pool = () => {
             <article className="px-5 pt-5 pb-6 mx-auto mt-5 border rounded-2xl">
               <div className="flex items-center justify-between">
                 <div className="flex items-center gap-1">
-                  <p>Token(ERC-1155) Contract Address</p>
+                  <h4 className="font-bold">
+                    Token(ERC-1155) Contract Address
+                  </h4>
                   {/* tooltip */}
                 </div>
+              </div>
+              <p className="mt-3 text-left text-black/30">
+                Please set additional values to apply to the basic formula in
+                Borrow Fee. Calculated based on open Interest and stop
+                profit/Loss rate.
+              </p>
+              <div className="flex justify-end gap-2 mt-6">
                 <div className="flex items-center gap-1">
                   {/* address */}
                   {selectedToken && (
@@ -168,15 +179,11 @@ const Pool = () => {
                   )}
                   <Button iconOnly={<Square2StackIcon />} />
                 </div>
-              </div>
-              <div>
-                <p className="mt-6 text-left text-black/30">
-                  Please set additional values to apply to the basic formula in
-                  Borrow Fee. Calculated based on open Interest and stop
-                  profit/Loss rate.
-                </p>
                 <Link to={"/trade"}>
-                  <Button label="Trade on this ETH/USDC Pool" />
+                  <Button
+                    label="Trade on ETH/USDC Pool"
+                    iconRight={<ChevronRightIcon />}
+                  />
                 </Link>
               </div>
             </article>
