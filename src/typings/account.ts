16e93869a5c4b895325a9d78ef166099431872eb
austin-builds <austin.31@quarkonix.com>
2023-06-19 16:09:49 +0900

    feat: account creation status

--- src/typings/account.ts
+++ src/typings/account.ts
@@ -2,3 +2,10 @@ export type Account = {
   walletAddress?: string;
   usumAddress?: string;
 };
+
+export const ACCOUNT_NONE = "NONE";
+export const ACCOUNT_CREATING = "CREATING";
+export const ACCOUNT_COMPLETING = "COMPLETING";
+export const ACCOUNT_COMPLETED = "COMPLETED";
+
+export type ACCOUNT_STATUS = "NONE" | "CREATING" | "COMPLETING" | "COMPLETED";
