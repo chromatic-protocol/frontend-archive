91204cd2a3a64111b48d31244d849655a92af1f7
austin-builds <austin.31@quarkonix.com>
2023-06-22 11:05:16 +0900

    refactor: token slice

--- src/store/reducer/index.ts
+++ src/store/reducer/index.ts
@@ -1,9 +1,11 @@
 import { marketReducer } from "./market";
 import { poolsReducer } from "./pools";
+import { tokenReducer } from "./token";
 
 const rootReducer = {
   market: marketReducer,
   pools: poolsReducer,
+  token: tokenReducer,
 };
 
 export default rootReducer;
--- src/store/reducer/market/index.ts
+++ src/store/reducer/market/index.ts
@@ -1,24 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
-import { Market, Token } from "../../../typings/market";
+import { Market } from "~/typings/market";
 
 interface MarketState {
   selectedMarket?: Market;
-  selectedToken?: Token;
 }
 
 const initialState: MarketState = {
   selectedMarket: undefined,
-  selectedToken: undefined,
 };
 
 export const marketSlice = createSlice({
   name: "market",
   initialState,
   reducers: {
-    onTokenSelect: (state, action: PayloadAction<Token>) => {
-      state.selectedToken = action.payload;
-    },
     onMarketSelect: (state, action: PayloadAction<Market>) => {
       state.selectedMarket = action.payload;
     },
