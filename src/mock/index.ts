abbf3a407a5e43123f6bac158f6c93ca2a4af790
austin-builds <austin.31@quarkonix.com>
2023-06-23 14:19:07 +0900

    fix: mockup data

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -1,5 +1,6 @@
 import { BigNumber } from "ethers";
 import { Market, Token } from "../typings/market";
+import { IOracleProvider } from "@chromatic-protocol/sdk/contracts";
 
 export const tokensMock: Token[] = [
   {
@@ -20,27 +21,27 @@ export const marketsMock: Record<string, Market[]> = {
       description: "ETH/USD",
       value: {
         price: BigNumber.from(1500),
-        timestamp: BigNumber.from(0),
-        version: BigNumber.from(0),
-      },
+        timestamp: BigNumber.from(1000000),
+        version: BigNumber.from(10),
+      } as IOracleProvider.OracleVersionStructOutput,
     },
     {
       address: "0x4445556667778889999",
       description: "AAVE/USD",
       value: {
         price: BigNumber.from(500),
-        timestamp: BigNumber.from(0),
-        version: BigNumber.from(0),
-      },
+        timestamp: BigNumber.from(1000000),
+        version: BigNumber.from(10),
+      } as IOracleProvider.OracleVersionStructOutput,
     },
     {
       address: "0x1111111111111111111",
       description: "GALA/USD",
       value: {
         price: BigNumber.from(200),
-        timestamp: BigNumber.from(0),
-        version: BigNumber.from(0),
-      },
+        timestamp: BigNumber.from(1000000),
+        version: BigNumber.from(10),
+      } as IOracleProvider.OracleVersionStructOutput,
     },
   ],
   USDT: [
@@ -48,19 +49,19 @@ export const marketsMock: Record<string, Market[]> = {
       address: "0x9999999999999999999",
       description: "ETH/USD",
       value: {
-        price: BigNumber.from(1500),
-        timestamp: BigNumber.from(0),
-        version: BigNumber.from(0),
-      },
+        price: BigNumber.from(750),
+        timestamp: BigNumber.from(1000000),
+        version: BigNumber.from(10),
+      } as IOracleProvider.OracleVersionStructOutput,
     },
     {
       address: "0x8888888888888888888",
       description: "ARB/USD",
       value: {
-        price: BigNumber.from(1500),
-        timestamp: BigNumber.from(0),
-        version: BigNumber.from(0),
-      },
+        price: BigNumber.from(500),
+        timestamp: BigNumber.from(1000000),
+        version: BigNumber.from(10),
+      } as IOracleProvider.OracleVersionStructOutput,
     },
   ],
 };
