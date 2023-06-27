96cc84f6949af4775e616ac8873a09fc2ce1b5e0
--global <--global>
2023-06-27 13:59:35 +0900

    feat: Update ChartTooltip for UI

--- src/stories/atom/ChartTooltip/stories.ts
+++ src/stories/atom/ChartTooltip/stories.ts
@@ -1,11 +1,11 @@
-import type { Meta, StoryObj } from "@storybook/react";
-import { ChartTooltip } from ".";
+import type { Meta, StoryObj } from '@storybook/react';
+import { ChartTooltip } from '.';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta = {
-  title: "Atom/ChartTooltip",
+  title: 'Atom/ChartTooltip',
   component: ChartTooltip,
-  tags: ["autodocs"],
+  tags: ['autodocs'],
   argTypes: {
     // backgroundColor: { control: "color" },
   },
@@ -17,16 +17,7 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Default: Story = {
   args: {
-    label: "1",
-    binPercent: 0.03,
-    liquidity: 100,
-    utilization: 30,
-  },
-};
-export const Selected: Story = {
-  args: {
-    label: "1",
-    makerMargin: 100,
-    selected: true,
+    label: '1',
+    tip: 'tooltip',
   },
 };
