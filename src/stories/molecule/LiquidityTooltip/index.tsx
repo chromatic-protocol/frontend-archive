87e585a98e6dd8de5fd4d1ab1d3406f37325022d
--global <--global>
2023-06-27 13:59:59 +0900

    feat: Update LiquidityTooltip contents

--- src/stories/molecule/LiquidityTooltip/index.tsx
+++ src/stories/molecule/LiquidityTooltip/index.tsx
@@ -1,41 +1,52 @@
-// import "./style.css";
-import { ChartTooltip } from "~/stories/atom/ChartTooltip";
+import { ChartTooltip } from '~/stories/atom/ChartTooltip';
 
-import { withComma } from "~/utils/number";
-import { isValid } from "~/utils/valid";
-
-// FIXME: @dia-nn 더 적절한 이름이 있다면 변경해주세요
+import { withComma } from '~/utils/number';
+import { isValid } from '~/utils/valid';
 
 export type LiquidityTooltipData = {
   feeRate: number;
   liquidity: number;
   utilization: number;
+  selected?: boolean;
 };
 
 //  Pool 페이지 차트 tooltip 컴포넌트
 interface LiquidityTooltipProps {
   getByIndex: (index: number) => LiquidityTooltipData;
-  index?: number;
+  index: number;
+  selected?: boolean;
 }
 
-export const LiquidityTooltip = ({
-  getByIndex,
-  index,
-}: LiquidityTooltipProps) => {
+export const LiquidityTooltip = ({ getByIndex, index, selected }: LiquidityTooltipProps) => {
   const { feeRate, liquidity, utilization } = getByIndex(index) ?? {};
 
-  const feeRateString = isValid(feeRate) ? feeRate.toString() : "-";
-  const liquidityString = isValid(liquidity) ? withComma(liquidity) : "-";
-  const utilizedString = isValid(utilization) ? withComma(utilization) : "-";
-  const ratio = liquidity !== 0 ? (utilization / liquidity).toString() : "-";
+  const feeRateString = isValid(feeRate) ? feeRate.toString() : '-';
+  const liquidityString = isValid(liquidity) ? withComma(liquidity) : '-';
+  const utilizedString = isValid(utilization) ? withComma(utilization) : '-';
+  const ratio = liquidity !== 0 ? (utilization / liquidity).toString() : '-';
 
   return (
-    <ChartTooltip>
-      <div>Liquidity Bin: {feeRateString}</div>
-      <div>Liquidity: {liquidityString}</div>
-      <div>
-        Utilization: {utilizedString} ({ratio}%)
-      </div>
-    </ChartTooltip>
+    // todo: 아래 label을 chart 내 hover 영역 className에 "chart-tooltip-${label}" 형식으로 동일하게 적용해야함
+    <ChartTooltip
+      label={isValid(index) ? index + 1 : 0}
+      tip={
+        selected ? (
+          // 선택된 영역 마우스오버시, Maker Margin 정보 보임
+          <div className={``}>
+            <p className="font-semibold text-black">Maker Margin:</p>
+          </div>
+        ) : (
+          <div className={``}>
+            <p className="font-semibold text-black">Liquidity Bin {feeRateString}</p>
+            <div className="flex flex-col gap-1 mt-2 text-sm font-semibold text-black/30">
+              <p>Liquidity: {liquidityString}</p>
+              <p>
+                Utilization: {utilizedString} ({ratio}%)
+              </p>
+            </div>
+          </div>
+        )
+      }
+    />
   );
 };
