ccb6d33639c463aa5c9e9b8a6561f965c71a62ed
kayden-fi <kayden.choi@quarkonix.com>
2023-06-28 13:28:47 +0900

    fix: add helper

---
+++ src/utils/promise.ts
@@ -0,0 +1,8 @@
+export async function PromiseOnlySuccess<T>(values: Iterable<T | PromiseLike<T>>) {
+  const result = await Promise.allSettled(values);
+  return (
+    result
+      .filter((v): v is PromiseFulfilledResult<Awaited<T>> => v.status === 'fulfilled')
+      .map(({ value }) => value) || ([] as T[])
+  );
+}
