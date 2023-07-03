95ea7ae71e160275acb69236cb6fa784752280d6
not-thing <dade.dd@quarkonix.com>
2023-07-03 16:27:17 +0900

    ci: configure build targets Fixes #219

--- src/constants/contracts.ts
+++ src/constants/contracts.ts
@@ -1,10 +1,12 @@
-import {
-  getAllDeployedContracts,
-  deployedAddress,
-} from "@chromatic-protocol/sdk/contracts";
+import { deployedAddress, getAllDeployedContracts } from '@chromatic-protocol/sdk/contracts';
 
-export const CHAIN = "anvil";
-
+export const CHAIN = import.meta.env.VITE_TARGET_CHAIN ?? 'anvil';
+const CHAIN_IDS = {
+  anvil: 31337,
+  arbitrum_goerli: 421613,
+  arbitrum_one: 42161,
+};
+export const CHAIN_ID: number = CHAIN_IDS[CHAIN] ?? CHAIN_IDS['anvil'];
 export const DEPLOYED_CONTRACTS = getAllDeployedContracts(CHAIN);
 
 export const DEPLOYED_ADDRESSES = deployedAddress[CHAIN];
