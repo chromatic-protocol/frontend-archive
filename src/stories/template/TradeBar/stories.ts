4a704450b9023ba222a0c9704595d0fecb4ef294
dia.nn <dia.nn@quarkonix.com>
2023-07-04 22:17:53 +0900

    wip: Set TradeBar stories args

--- src/stories/template/TradeBar/stories.ts
+++ src/stories/template/TradeBar/stories.ts
@@ -1,12 +1,15 @@
-import type { Meta, StoryObj } from "@storybook/react";
-import { TradeBar } from ".";
+import type { Meta, StoryObj } from '@storybook/react';
+import { TradeBar } from '.';
+import { BigNumber } from 'ethers';
+import { Market } from '~/typings/market';
+import { Position } from '../../../hooks/usePosition';
 
 const meta = {
-  title: "Template/TradeBar",
+  title: 'Template/TradeBar',
   component: TradeBar,
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
-    layout: "fullscreen",
+    layout: 'fullscreen',
   },
 } satisfies Meta<typeof TradeBar>;
 
@@ -14,5 +17,25 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Empty: Story = {
-  
+  args: {
+    loading: false,
+    token: {
+      name: 'USDC',
+      address: '0x8888888888888888888888888888888888888888',
+      decimals: 6,
+    },
+    markets: [
+      {
+        address: '0x8888888888888888888888888888888888888888',
+        description: 'ETH/USD',
+        oracleValue: {
+          price: BigNumber.from(10000),
+          timestamp: BigNumber.from(1000000),
+          version: BigNumber.from(10),
+        },
+      } as Market,
+    ],
+    // positions: {},
+    // oracleVersions?: Record<string, OracleVersion>;
+  },
 };
