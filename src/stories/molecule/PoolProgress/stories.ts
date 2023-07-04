e75a5dd41558ae79e17c48b75d560c31a599a91c
dia.nn <dia.nn@quarkonix.com>
2023-07-04 22:24:03 +0900

    wip: Set PoolProgress stories args

--- src/stories/molecule/PoolProgress/stories.ts
+++ src/stories/molecule/PoolProgress/stories.ts
@@ -1,8 +1,10 @@
-import type { Meta, StoryObj } from "@storybook/react";
-import { PoolProgress } from ".";
+import type { Meta, StoryObj } from '@storybook/react';
+import { PoolProgress } from '.';
+import { BigNumber } from 'ethers';
+import { Market } from '~/typings/market';
 
 const meta = {
-  title: "Molecule/PoolProgress",
+  title: 'Molecule/PoolProgress',
   component: PoolProgress,
 } satisfies Meta<typeof PoolProgress>;
 
@@ -11,6 +13,20 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    //
+    loading: false,
+    token: {
+      name: 'USDC',
+      address: '0x8888888888888888888888888888888888888888',
+      decimals: 6,
+    },
+    market: {
+      address: '0x8888888888888888888888888888888888888888',
+      description: 'ETH/USD',
+      oracleValue: {
+        price: BigNumber.from(10000),
+        timestamp: BigNumber.from(1000000),
+        version: BigNumber.from(10),
+      },
+    } as Market,
   },
 };
