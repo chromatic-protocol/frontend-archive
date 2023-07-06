7c098b749e265fba8a9726ab112d08a0947ac5c2
austin-builds <austin.31@quarkonix.com>
2023-07-06 17:35:19 +0900

    refactor: hooks to close and claim positions

---
+++ src/hooks/useClosePosition.ts
@@ -0,0 +1,49 @@
+import { BigNumber } from 'ethers';
+import { useChromaticClient } from './useChromaticClient';
+import { errorLog } from '~/utils/log';
+import { AppError } from '~/typings/error';
+import { isNil } from 'ramda';
+import { toast } from 'react-toastify';
+import { usePosition } from './usePosition';
+
+interface Props {
+  marketAddress: string;
+  positionId: BigNumber;
+}
+
+function useClosePosition(props: Props) {
+  const { marketAddress, positionId } = props;
+  const { client } = useChromaticClient();
+  const { positions, fetchPositions } = usePosition();
+
+  const onClosePosition = async function () {
+    if (isNil(client?.router())) {
+      errorLog('no router contracts');
+      return AppError.reject('no router contracts', 'onClosePosition');
+    }
+    const position = positions?.find(
+      (position) => position.marketAddress === marketAddress && position.id === positionId
+    );
+    if (isNil(position)) {
+      errorLog('no positions');
+      toast('Position is not selected.');
+      return AppError.reject('no positions', 'onClosePosition');
+    }
+    try {
+      await client?.router()?.closePosition(position.marketAddress, position.id);
+
+      fetchPositions();
+    } catch (error) {
+      errorLog(error);
+      toast('Position was not deleted, Found error.');
+
+      return AppError.reject(error, 'onClosePosition');
+    }
+  };
+
+  return {
+    onClosePosition,
+  };
+}
+
+export { useClosePosition };
