143ec1e6fa500e2ef946e5aa15111948d68ba054
dia.nn <dia.nn@quarkonix.com>
2023-07-10 16:52:15 +0900

    style: Connect button in Header

--- src/stories/atom/Avatar/index.tsx
+++ src/stories/atom/Avatar/index.tsx
@@ -1,12 +1,12 @@
-import "./style.css";
+import './style.css';
 
 interface AvatarProps {
   label?: string;
   active?: boolean;
   backgroundColor?: string;
-  size?: "xs" | "sm" | "base" | "lg" | "xl";
-  fontSize?: "xs" | "sm" | "base" | "lg" | "xl";
-  fontWeight?: "lighter" | "normal" | "medium" | "semibold" | "bold";
+  size?: 'xs' | 'sm' | 'base' | 'lg' | 'xl';
+  fontSize?: 'xs' | 'sm' | 'base' | 'lg' | 'xl';
+  fontWeight?: 'lighter' | 'normal' | 'medium' | 'semibold' | 'bold';
   gap?: string;
   className?: string;
   disabled?: boolean;
@@ -17,12 +17,12 @@ interface AvatarProps {
 export const Avatar = (props: AvatarProps) => {
   const {
     className,
-    size = "base",
-    gap = "2",
+    size = 'base',
+    gap = '2',
     src,
     label,
-    fontSize = "base",
-    fontWeight = "semibold",
+    fontSize = 'base',
+    fontWeight = 'semibold',
   } = props;
 
   return (
@@ -31,13 +31,9 @@ export const Avatar = (props: AvatarProps) => {
         <div
           className={`avatar-${size} ${className} bg-[#D9D9D9] rounded-full overflow-hidden shrink-0`}
         >
-          {src && (
-            <img src={src} alt="" className="object-cover w-full h-full" />
-          )}
+          {src && <img src={src} alt="" className="object-cover w-full h-full" />}
         </div>
-        {label && (
-          <p className={`font-${fontWeight} text-${fontSize}`}>{label}</p>
-        )}
+        {label && <p className={`font-${fontWeight} text-${fontSize}`}>{label}</p>}
       </div>
     </div>
   );
