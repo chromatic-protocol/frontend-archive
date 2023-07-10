9233e95db624886929edac27c2c18c037d82fe53
dia.nn <dia.nn@quarkonix.com>
2023-07-10 17:12:32 +0900

    style: update Connect button details

--- src/stories/template/Header/index.tsx
+++ src/stories/template/Header/index.tsx
@@ -91,10 +91,10 @@ export const Header = (props: HeaderProps) => {
               <button
                 onClick={onConnect}
                 title="connect"
-                className="p-[2px] flex items-center border rounded-full bg-black border-grayL text-white min-w-[175px]"
+                className="p-[2px] flex items-center border rounded-full bg-black border-grayL text-white min-w-[148px]"
               >
                 <Avatar src={arbitrumIcon} className="!w-[36px] !h-[36px]" />
-                <p className="w-full pr-5 text-lg font-normal text-center">Connect</p>
+                <p className="w-full pr-4 text-lg font-semibold text-center">Connect</p>
               </button>
             </>
           )}
