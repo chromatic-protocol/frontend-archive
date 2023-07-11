cc3fdb7ab85a45342a2a14e7548b09883ad92d5f
dia.nn <dia.nn@quarkonix.com>
2023-07-11 15:50:59 +0900

    Update Button prop - href, to

--- src/stories/atom/Button/index.tsx
+++ src/stories/atom/Button/index.tsx
@@ -1,11 +1,13 @@
-import { MouseEventHandler } from "react";
-import "./style.css";
+import { MouseEventHandler } from 'react';
+import './style.css';
 
 interface ButtonProps {
   label?: string;
-  css?: "default" | "active" | "gray" | "unstyled" | "circle";
-  size?: "xs" | "sm" | "base" | "lg" | "xl" | "2xl";
-  align?: "center" | "left" | "right";
+  css?: 'default' | 'active' | 'gray' | 'unstyled' | 'circle';
+  size?: 'xs' | 'sm' | 'base' | 'lg' | 'xl' | '2xl';
+  align?: 'center' | 'left' | 'right';
+  to?: string;
+  href?: string;
   gap?: string;
   className?: string;
   iconLeft?: any;
@@ -18,9 +20,11 @@ interface ButtonProps {
 export const Button = (props: ButtonProps) => {
   const {
     label,
-    css = "default",
-    size = "base",
-    align = "center",
+    css = 'default',
+    size = 'base',
+    align = 'center',
+    to,
+    href,
     gap,
     className,
     iconLeft,
@@ -29,20 +33,28 @@ export const Button = (props: ButtonProps) => {
     disabled = false,
     onClick,
   } = props;
-  const btnIconOnly = iconOnly ? "btn-icon-only" : "";
+  const btnIconOnly = iconOnly ? 'btn-icon-only' : '';
 
   return (
-    <button
-      type="button"
-      className={`btn btn-${size} btn-${css} ${className} ${btnIconOnly}`}
-      disabled={disabled}
-      onClick={onClick}
-    >
-      <div className={`flex items-center gap-${gap} justify-${align}`}>
-        {iconLeft !== undefined ? iconLeft : null}
-        {iconOnly !== undefined ? iconOnly : label}
-        {iconRight !== undefined ? iconRight : null}
-      </div>
-    </button>
+    <>
+      <button
+        type="button"
+        className={`btn btn-${size} btn-${css} ${className} ${btnIconOnly}`}
+        disabled={disabled}
+        onClick={
+          href
+            ? () => window.open(href, '_blank')
+            : to
+            ? () => (window.location.href = to)
+            : onClick
+        }
+      >
+        <div className={`flex items-center gap-${gap} justify-${align}`}>
+          {iconLeft !== undefined ? iconLeft : null}
+          {iconOnly !== undefined ? iconOnly : label}
+          {iconRight !== undefined ? iconRight : null}
+        </div>
+      </button>
+    </>
   );
 };
