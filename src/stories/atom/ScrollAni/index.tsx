e394bdf504b1366891c178b484d0771b235fd7d4
dia.nn <dia.nn@quarkonix.com>
2023-07-13 10:27:38 +0900

    wip: ScrollAni effect - now: it disappear in 2 sec - tobe: when scroll it disappear, when goes top it appears again

--- src/stories/atom/ScrollAni/index.tsx
+++ src/stories/atom/ScrollAni/index.tsx
@@ -1,13 +1,25 @@
-import { MouseEventHandler } from "react";
-import { ChevronDoubleDownIcon } from "@heroicons/react/24/outline";
+import React, { useEffect, useState } from 'react';
+import { ChevronDoubleDownIcon } from '@heroicons/react/24/outline';
+import './style.css';
 
 interface ScrollAniProps {
   disabled?: boolean;
-  onClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
-export const ScrollAni = (props: ScrollAniProps) => {
-  // const { onClick } = props;
+export const ScrollAni: React.FC<ScrollAniProps> = (props: ScrollAniProps) => {
+  const [isVisible, setIsVisible] = useState(true);
 
-  return <ChevronDoubleDownIcon className="w-4 animate-bounce scroll-ani" />;
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div className={`scroll-ani transition-opacity ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
+      <ChevronDoubleDownIcon className="w-4 animate-bounce" />
+    </div>
+  );
 };
