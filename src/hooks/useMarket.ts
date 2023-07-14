986408ef2ec4a489403550ad6ab767d0f64ff45b
kayden-fi <kayden.choi@quarkonix.com>
2023-07-14 18:40:42 +0900

    The token address copy button is copying an incorrect CLB token address. Fixes #329

--- src/hooks/useMarket.ts
+++ src/hooks/useMarket.ts
@@ -1,32 +1,47 @@
-import { useCallback } from 'react';
+import { useCallback, useMemo } from 'react';
+import { toast } from 'react-toastify';
 import useSWR from 'swr';
 import { useAppDispatch, useAppSelector } from '~/store';
 import { marketAction } from '~/store/reducer/market';
 import { Market } from '~/typings/market';
-import { isValid } from '~/utils/valid';
+import { checkAllProps } from '../utils';
 import { useChromaticClient } from './useChromaticClient';
-import useLocalStorage from './useLocalStorage';
-import { toast } from 'react-toastify';
 import { useError } from './useError';
+import useLocalStorage from './useLocalStorage';
 
 export const useMarket = (_interval?: number) => {
   const { client } = useChromaticClient();
   const selectedToken = useAppSelector((state) => state.token.selectedToken);
   const currentMarket = useAppSelector((state) => state.market.selectedMarket);
-
+  const marketFactoryApi = useMemo(() => client?.marketFactory(), [client]);
+  const marketApi = useMemo(() => client?.market(), [client]);
   const dispatch = useAppDispatch();
   const { setState: setStoredMarket } = useLocalStorage('usum:market');
-
+  const marketsRequired = {
+    marketFactoryApi: marketFactoryApi,
+    selectedTokenAddress: selectedToken?.address,
+  };
   const {
     data: markets,
-    error,
+    error: marketsError,
     mutate: fetchMarkets,
     isLoading: isMarketLoading,
   } = useSWR(
-    isValid(selectedToken) ? ['MARKET', selectedToken.address] : undefined,
-    async ([_, tokenAddress]) => {
-      const markets = (await client?.marketFactory().getMarkets(tokenAddress)) || [];
+    checkAllProps(marketsRequired) ? marketsRequired : null,
+    async ({ marketFactoryApi, selectedTokenAddress }) => {
+      const markets = (await marketFactoryApi.getMarkets(selectedTokenAddress)) || [];
       return markets;
+    }
+  );
+
+  const getClbTokenAddressRequired = {
+    currentMarket: currentMarket,
+    marketApi: marketApi,
+  };
+  const { data: clbTokenAddress, error } = useSWR(
+    checkAllProps(getClbTokenAddressRequired) ? getClbTokenAddressRequired : null,
+    async ({ marketApi, currentMarket }) => {
+      return marketApi.clbToken(currentMarket.address);
     }
   );
 
@@ -41,5 +56,12 @@ export const useMarket = (_interval?: number) => {
     [dispatch]
   );
 
-  return { markets, currentMarket, isMarketLoading, fetchMarkets, onMarketSelect } as const;
+  return {
+    clbTokenAddress,
+    markets,
+    currentMarket,
+    isMarketLoading,
+    fetchMarkets,
+    onMarketSelect,
+  } as const;
 };
