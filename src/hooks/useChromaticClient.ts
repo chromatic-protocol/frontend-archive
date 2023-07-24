62d7d952c1014afb66741faad6a4fc1b9fa7ca8d
jaycho-46 <jay.46@quarkonix.com>
2023-07-25 00:36:56 +0900

    Add client store with redux

--- src/hooks/useChromaticClient.ts
+++ src/hooks/useChromaticClient.ts
@@ -1,66 +1,56 @@
 import { Client } from '@chromatic-protocol/sdk-viem';
-import { useEffect, useRef, useSyncExternalStore } from 'react';
-import { PublicClient, WalletClient, useAccount, usePublicClient, useWalletClient } from 'wagmi';
+import { useEffect, useRef, useSyncExternalStore, useMemo } from 'react';
+import { PublicClient, WalletClient, usePublicClient, useWalletClient, useAccount } from 'wagmi';
 import { CHAIN_ID } from '~/constants';
 import { Logger } from '../utils/log';
+import { useAppDispatch, useAppSelector } from '~/store';
+import { clientAction } from '~/store/reducer/client';
 
-const logger = Logger('useChromaticClient');
-let chromaticClient: Client | undefined;
-let listeners: Array<() => void> = [];
-function emitChanges() {
-  for (const listener of listeners) {
-    listener();
-  }
-}
-const clientStore = {
-  updateWalletClient(walletClient: WalletClient | undefined) {
-    if (!chromaticClient) chromaticClient = new Client();
-    chromaticClient.walletClient = walletClient;
-    emitChanges();
-  },
-  updatePublicClient(publicClient: PublicClient) {
-    if (!chromaticClient) chromaticClient = new Client();
-    chromaticClient.publicClient = publicClient;
-    emitChanges();
-  },
-  subscribe(listener: () => void) {
-    listeners.push(listener);
-    return () => {
-      listeners = listeners.filter((l) => l !== listener);
-    };
-  },
-  getSnapshot() {
-    return chromaticClient;
-  },
-};
+// const logger = Logger('useChromaticClient');
+// let chromaticClient: Client | undefined;
+// let listeners: Array<() => void> = [];
+// function emitChanges() {
+//   for (const listener of listeners) {
+//     listener();
+//   }
+// }
+// const clientStore = {
+//   updateWalletClient(walletClient: WalletClient | undefined) {
+//     if (!chromaticClient) chromaticClient = new Client();
+//     chromaticClient.walletClient = walletClient;
+
+//     emitChanges();
+//   },
+//   updatePublicClient(publicClient: PublicClient) {
+//     if (!chromaticClient) chromaticClient = new Client();
+//     chromaticClient.publicClient = publicClient;
+//     emitChanges();
+//   },
+//   subscribe(listener: () => void) {
+//     listeners.push(listener);
+//     return () => {
+//       listeners = listeners.filter((l) => l !== listener);
+//     };
+//   },
+//   getSnapshot() {
+//     return chromaticClient
+//   },
+// };
 
 export function useChromaticClient() {
   const publicClient = usePublicClient({ chainId: CHAIN_ID });
   const { data: walletClient } = useWalletClient({ chainId: CHAIN_ID });
-  const { address, isConnected } = useAccount();
-  const previousClient = useRef(chromaticClient);
-  const client = useSyncExternalStore(clientStore.subscribe, clientStore.getSnapshot);
+
+  const client = useAppSelector(({ client }) => client);
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
-    if (!chromaticClient) chromaticClient = new Client({ publicClient, walletClient });
-    if (client) {
-      clientStore.updatePublicClient(publicClient);
-      previousClient.current = client;
-      if (
-        walletClient &&
-        walletClient.account &&
-        address &&
-        isConnected &&
-        previousClient.current.walletClient !== walletClient
-      ) {
-        logger.info('update wallet client', walletClient);
-        clientStore.updateWalletClient(walletClient);
-        previousClient.current = client;
-      }
-    }
-  }, [publicClient, walletClient, isConnected, address]);
+    dispatch(clientAction.updatePublicClient({ publicClient }));
+  }, [publicClient]);
 
-  return {
-    client,
-  };
+  useEffect(() => {
+    dispatch(clientAction.updateWalletClient({ walletClient: walletClient as WalletClient }));
+  }, [walletClient]);
+
+  return client;
 }
--- src/store/reducer/index.ts
+++ src/store/reducer/index.ts
@@ -1,11 +1,13 @@
-import { marketReducer } from "./market";
-import { poolsReducer } from "./pools";
-import { tokenReducer } from "./token";
+import { marketReducer } from './market';
+import { poolsReducer } from './pools';
+import { tokenReducer } from './token';
+import { clientReducer } from './client';
 
 const rootReducer = {
   market: marketReducer,
   pools: poolsReducer,
   token: tokenReducer,
+  client: clientReducer,
 };
 
 export default rootReducer;
