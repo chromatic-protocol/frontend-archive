009f8a4d3834328945c5982df1b79d5ff98323aa
dia.nn <dia.nn@quarkonix.com>
2023-07-26 12:05:57 +0900

    feat: Add Wallet button - Network err state - refactor wallet button style

--- src/stories/template/Header/index.tsx
+++ src/stories/template/Header/index.tsx
@@ -6,6 +6,8 @@ import { LiquidityPoolSummary } from '~/typings/pools';
 import { isValid } from '../../../utils/valid';
 import { Avatar } from '../../atom/Avatar';
 import { WalletPopover } from '../../molecule/WalletPopover';
+import { ExclamationTriangleIcon } from '@heroicons/react/24/outline';
+import { TooltipAlert } from '~/stories/atom/TooltipAlert';
 
 import arbitrumIcon from '/src/assets/images/arbitrum.svg';
 import { Address } from 'wagmi';
@@ -85,15 +87,34 @@ export const Header = (props: HeaderProps) => {
                 onWalletCopy={onWalletCopy}
                 onUsumCopy={onUsumCopy}
               />
+              {/* todo: Wrong Network */}
+              {/* when button clicked, wallet popup(change network) is open. */}
+              <button
+                // onClick={}
+                title="change network"
+                className="tooltip-change-network min-w-[175px] btn-wallet"
+              >
+                <Avatar
+                  // label={account?.walletAddress && trimAddress(account?.walletAddress, 7, 5)}
+                  label="0x00000...00000"
+                  svg={<ExclamationTriangleIcon />}
+                  className="text-black avatar"
+                  fontSize="sm"
+                  fontWeight="normal"
+                  gap="3"
+                />
+                <TooltipAlert
+                  label="change-network"
+                  tip="Change Network"
+                  place="left"
+                  css="outline"
+                />
+              </button>
             </>
           ) : (
             <>
-              <button
-                onClick={onConnect}
-                title="connect"
-                className="p-[2px] flex items-center border rounded-full bg-black border-grayL text-white min-w-[148px]"
-              >
-                <Avatar src={arbitrumIcon} className="!w-[36px] !h-[36px]" />
+              <button onClick={onConnect} title="connect" className="btn-wallet min-w-[148px]">
+                <Avatar src={arbitrumIcon} className="avatar" />
                 <p className="w-full pr-4 text-lg font-semibold text-center">Connect</p>
               </button>
             </>
