caa6b4e32a72384253cd640b187364a5a822c9f7
jaycho-46 <jay.46@quarkonix.com>
2023-07-27 00:11:32 +0900

    Remove 'wagmi test'

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,29 +2,11 @@ import React from 'react';
 import { Navigate, createBrowserRouter } from 'react-router-dom';
 import Pool from './pages/pool';
 import Trade from './pages/trade';
-import WagmiTest from './components/WagmiTest';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Navigate to={'/trade'} />,
-  },
-  {
-    path: '/hello',
-    element: (
-      <div>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      </div>
-    ),
-  },
-  {
-    path: '/wagmi',
-    element: (
-      <div>
-        <h1 className="text-3xl font-bold underline">Wagmi Test</h1>
-        <WagmiTest />
-      </div>
-    ),
   },
   {
     path: '/pool',
