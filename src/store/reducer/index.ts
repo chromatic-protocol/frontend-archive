4b2d56a7294c17c71123a1d58ec1c153a59d920f
jaycho-46 <jay.46@quarkonix.com>
2023-07-27 23:59:01 +0900

    Remove client reducer

--- src/store/reducer/index.ts
+++ src/store/reducer/index.ts
@@ -1,13 +1,11 @@
 import { marketReducer } from './market';
 import { poolsReducer } from './pools';
 import { tokenReducer } from './token';
-import { clientReducer } from './client';
 
 const rootReducer = {
   market: marketReducer,
   pools: poolsReducer,
   token: tokenReducer,
-  client: clientReducer,
 };
 
 export default rootReducer;
