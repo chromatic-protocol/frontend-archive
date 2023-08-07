9601132fa2d03810cb37be446b60c9e16b5a48d2
dia.nn <dia.nn@quarkonix.com>
2023-08-07 18:24:30 +0900

    style: update darkmode Buttons

--- src/stories/atom/PopoverArrow/index.tsx
+++ src/stories/atom/PopoverArrow/index.tsx
@@ -16,7 +16,7 @@ export const PopoverArrow = (props: PopoverArrowProps) => {
 
   return (
     <span
-      className={`inline-block relative border-none w-[53px] h-[53px] rounded-full text-black3 hover:text-black2 overflow-hidden cursor-pointer ${className}`}
+      className={`inline-block relative border-none w-[53px] h-[53px] rounded-full text-black3 hover:text-black2 dark:text-white3 dark:hover:text-white2 overflow-hidden cursor-pointer ${className}`}
       onClick={onClick}
     >
       <span className="inline-flex items-center justify-center w-full h-full">
@@ -64,18 +64,16 @@ const PopoverLineBg = (props: PopoverLineBgProps) => {
       }`}
     >
       <path
-        fill="#EEE"
         fillRule="evenodd"
         d="M17 53h1c0-3.816 3.234-6.983 7.184-8.497C32.665 41.638 38 34.92 38 27c0-7.919-5.335-14.638-12.816-17.503C21.235 7.983 18 4.817 18 1V0h-1v1c0 4.418 3.7 7.85 7.827 9.43C31.983 13.172 37 19.56 37 27c0 7.441-5.017 13.828-12.173 16.57C20.7 45.15 17 48.581 17 53Z"
         clipRule="evenodd"
-        className="translate-x-1"
+        className="translate-x-1 fill-light1 dark:fill-dark1"
       />
       <path
-        fill="#FFF"
         fillRule="evenodd"
         d="M17 0H0v53h17c0-4.418 3.7-7.85 7.827-9.43C31.983 40.828 37 34.44 37 27c0-7.441-5.017-13.828-12.173-16.57C20.7 8.85 17 5.419 17 1V0Z"
         clipRule="evenodd"
-        className="translate-x-1"
+        className="translate-x-1 fill-white dark:fill-black1"
       />
     </svg>
   );
