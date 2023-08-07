e3f4f38266b47f88578a455034084b0b702f2089
austin-builds <austin.31@quarkonix.com>
2023-08-07 11:10:51 +0900

    fix: show modal to notify of wrong chains

--- src/stories/container/Header/index.tsx
+++ src/stories/container/Header/index.tsx
@@ -1,4 +1,5 @@
 import { useAccount, useConnect } from 'wagmi';
+import { useChainListener } from '~/hooks/useChainListener';
 import { useChromaticAccount } from '~/hooks/useChromaticAccount';
 import { Header as HeaderPresenter } from '~/stories/template/Header';
 
@@ -6,10 +7,12 @@ export const Header = () => {
   const { address: walletAddress } = useAccount();
   const { accountAddress: chromaticAddress } = useChromaticAccount();
   const { connectAsync, connectors } = useConnect();
+  const { isSameChain } = useChainListener();
 
   return (
     <HeaderPresenter
       account={{ walletAddress, chromaticAddress }}
+      isSameChain={isSameChain}
       onConnect={() => {
         connectAsync({ connector: connectors[0] });
       }}
