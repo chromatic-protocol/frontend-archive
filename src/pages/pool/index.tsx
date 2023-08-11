33fec9c581ab504be036c880f357d53eb5a33a03
dia.nn <dia.nn@quarkonix.com>
2023-08-11 15:33:04 +0900

    wip: z-index in pages

--- src/pages/pool/index.tsx
+++ src/pages/pool/index.tsx
@@ -41,67 +41,64 @@ const Pool = () => {
 
   return (
     <div className="flex flex-col min-h-[100vh] w-full bg-paper-lightest">
-      <div className="z-0">
-        <Header />
-        <section className="flex flex-col grow w-full max-w-[1200px] px-5 mx-auto mb-20">
-          <MainBar showAccountPopover={false} />
-          <div className="flex items-stretch gap-3">
-            <div className="flex-auto w-3/5">
-              <PoolPanel />
-              {/* bottom */}
-              <article className="p-5 mx-auto mt-5 border shadow-lg dark:shadow-none bg-paper rounded-2xl">
-                <div className="flex items-center justify-between w-full gap-1">
-                  <h4 className="font-bold">Token(ERC-1155) Contract Address</h4>
-                  <div className="flex gap-2">
-                    <AddressCopyButton
-                      address={clbTokenAddress && trimAddress(clbTokenAddress, 6, 6)}
-                      onClick={() => {
-                        if (clbTokenAddress) {
-                          copyText(clbTokenAddress);
-                        }
-                      }}
-                    />
-                    {/* todo : outlink button link */}
-                    <Button
-                      href={
-                        clbTokenAddress && blockExplorer
-                          ? `${blockExplorer}/token/${clbTokenAddress}`
-                          : undefined
+      <Header />
+      <section className="flex flex-col grow w-full max-w-[1200px] px-5 mx-auto mb-20">
+        <MainBar showAccountPopover={false} />
+        <div className="flex items-stretch gap-3">
+          <div className="flex-auto w-3/5">
+            <PoolPanel />
+            {/* bottom */}
+            <article className="p-5 mx-auto mt-5 border shadow-lg dark:shadow-none bg-paper rounded-2xl">
+              <div className="flex items-center justify-between w-full gap-1">
+                <h4 className="font-bold">Token(ERC-1155) Contract Address</h4>
+                <div className="flex gap-2">
+                  <AddressCopyButton
+                    address={clbTokenAddress && trimAddress(clbTokenAddress, 6, 6)}
+                    onClick={() => {
+                      if (clbTokenAddress) {
+                        copyText(clbTokenAddress);
                       }
-                      label="view scanner"
-                      css="circle"
-                      size="lg"
-                      iconOnly={<ArrowTopRightOnSquareIcon />}
-                    />
-                  </div>
-                </div>
-                <div className="mt-3 mb-3 text-base text-left text-primary-lighter">
-                  When providing liquidity to the liquidity bins of the Chromatic protocol,
-                  providers are rewarded by minting CLB tokens. CLB tokens follow the ERC-1155
-                  standard and have one token contract per market, with each bin having its own
-                  unique token ID.{' '}
-                  <Outlink outLink="https://chromatic-protocol.gitbook.io/docs/tokens/clb-token-erc-1155" />
-                </div>
-              </article>
-              <div className="mt-10">
-                <Link to={'/trade'}>
-                  <Button
-                    label={
-                      isValid(selectedMarket)
-                        ? `Trade on ${selectedMarket.description} Pool`
-                        : 'Market loading'
-                    }
-                    iconRight={<ChevronRightIcon />}
+                    }}
                   />
-                </Link>
+                  {/* todo : outlink button link */}
+                  <Button
+                    href={
+                      clbTokenAddress && blockExplorer
+                        ? `${blockExplorer}/token/${clbTokenAddress}`
+                        : undefined
+                    }
+                    label="view scanner"
+                    css="circle"
+                    size="lg"
+                    iconOnly={<ArrowTopRightOnSquareIcon />}
+                  />
+                </div>
               </div>
-            </div>
-            <div className="w-2/5 max-w-[500px] min-w-[480px]">
-              <PoolProgress />
+              <div className="mt-3 mb-3 text-base text-left text-primary-lighter">
+                When providing liquidity to the liquidity bins of the Chromatic protocol, providers
+                are rewarded by minting CLB tokens. CLB tokens follow the ERC-1155 standard and have
+                one token contract per market, with each bin having its own unique token ID.{' '}
+                <Outlink outLink="https://chromatic-protocol.gitbook.io/docs/tokens/clb-token-erc-1155" />
+              </div>
+            </article>
+            <div className="mt-10">
+              <Link to={'/trade'}>
+                <Button
+                  label={
+                    isValid(selectedMarket)
+                      ? `Trade on ${selectedMarket.description} Pool`
+                      : 'Market loading'
+                  }
+                  iconRight={<ChevronRightIcon />}
+                />
+              </Link>
             </div>
           </div>
-        </section>
-      </div>
+          <div className="w-2/5 max-w-[500px] min-w-[480px]">
+            <PoolProgress />
+          </div>
+        </div>
+      </section>
       <Footer />
       <Toast />
       <ChainModal />
--- src/pages/trade/index.tsx
+++ src/pages/trade/index.tsx
@@ -18,29 +18,26 @@ const Trade = () => {
 
   return (
     <div className="flex flex-col min-h-[100vh] w-full bg-paper-lightest">
-      <div className="z-0">
-        <Header />
-        <section className="flex flex-col grow w-full max-w-[1200px] items-stretch px-5 mx-auto mb-20">
-          <MainBar showAccountPopover={true} />
-          <div className="w-full">
-            <TradePanel />
-            <article className="w-full mx-auto mt-8 max-w-[840px]">
-              <div className="mb-12 text-base">
-                <div className="my-6 text-center text-primary-lighter">
-                  The Trade Fee is calculated by summing up the different fees from the Liquidity
-                  Bins that accept the positions. The EST. Trade Fee is calculated based on the
-                  current oracle price, and the actual fee paid is determined by the next oracle
-                  price.{' '}
-                  <Outlink outLink="https://chromatic-protocol.gitbook.io/docs/fee/trading-fee" />
-                </div>
-                <Link to={'/pool'}>
-                  <Button label="Provide Liquidity" iconRight={<ChevronRightIcon />} />
-                </Link>
+      <Header />
+      <section className="flex flex-col grow w-full max-w-[1200px] items-stretch px-5 mx-auto mb-20">
+        <MainBar showAccountPopover={true} />
+        <div className="w-full">
+          <TradePanel />
+          <article className="w-full mx-auto mt-8 max-w-[840px]">
+            <div className="mb-12 text-base">
+              <div className="my-6 text-center text-primary-lighter">
+                The Trade Fee is calculated by summing up the different fees from the Liquidity Bins
+                that accept the positions. The EST. Trade Fee is calculated based on the current
+                oracle price, and the actual fee paid is determined by the next oracle price.{' '}
+                <Outlink outLink="https://chromatic-protocol.gitbook.io/docs/fee/trading-fee" />
               </div>
-            </article>
-          </div>
-        </section>
-      </div>
+              <Link to={'/pool'}>
+                <Button label="Provide Liquidity" iconRight={<ChevronRightIcon />} />
+              </Link>
+            </div>
+          </article>
+        </div>
+      </section>
       <TradeBar />
       <Toast />
       {/* todo: Add Modal (Wrong Network) */}
