b6876e92711b68e49cc480f5d7ae4b4f8a950143
dia.nn <dia.nn@quarkonix.com>
2023-08-11 10:55:20 +0900

    fix: z-index

--- src/pages/pool/index.tsx
+++ src/pages/pool/index.tsx
@@ -41,64 +41,67 @@ const Pool = () => {
 
   return (
     <div className="flex flex-col min-h-[100vh] w-full bg-paper-lightest">
-      <Header />
-      <section className="flex flex-col grow w-full max-w-[1200px] px-5 mx-auto mb-20 z-0">
-        <MainBar showAccountPopover={false} />
-        <div className="flex items-stretch gap-3">
-          <div className="flex-auto w-3/5">
-            <PoolPanel />
-            {/* bottom */}
-            <article className="p-5 mx-auto mt-5 border shadow-lg dark:shadow-none bg-paper rounded-2xl">
-              <div className="flex items-center justify-between w-full gap-1">
-                <h4 className="font-bold">Token(ERC-1155) Contract Address</h4>
-                <div className="flex gap-2">
-                  <AddressCopyButton
-                    address={clbTokenAddress && trimAddress(clbTokenAddress, 6, 6)}
-                    onClick={() => {
-                      if (clbTokenAddress) {
-                        copyText(clbTokenAddress);
+      <div className="z-0">
+        <Header />
+        <section className="flex flex-col grow w-full max-w-[1200px] px-5 mx-auto mb-20">
+          <MainBar showAccountPopover={false} />
+          <div className="flex items-stretch gap-3">
+            <div className="flex-auto w-3/5">
+              <PoolPanel />
+              {/* bottom */}
+              <article className="p-5 mx-auto mt-5 border shadow-lg dark:shadow-none bg-paper rounded-2xl">
+                <div className="flex items-center justify-between w-full gap-1">
+                  <h4 className="font-bold">Token(ERC-1155) Contract Address</h4>
+                  <div className="flex gap-2">
+                    <AddressCopyButton
+                      address={clbTokenAddress && trimAddress(clbTokenAddress, 6, 6)}
+                      onClick={() => {
+                        if (clbTokenAddress) {
+                          copyText(clbTokenAddress);
+                        }
+                      }}
+                    />
+                    {/* todo : outlink button link */}
+                    <Button
+                      href={
+                        clbTokenAddress && blockExplorer
+                          ? `${blockExplorer}/token/${clbTokenAddress}`
+                          : undefined
                       }
-                    }}
-                  />
-                  {/* todo : outlink button link */}
-                  <Button
-                    href={
-                      clbTokenAddress && blockExplorer
-                        ? `${blockExplorer}/token/${clbTokenAddress}`
-                        : undefined
-                    }
-                    label="view scanner"
-                    css="circle"
-                    size="lg"
-                    iconOnly={<ArrowTopRightOnSquareIcon />}
-                  />
+                      label="view scanner"
+                      css="circle"
+                      size="lg"
+                      iconOnly={<ArrowTopRightOnSquareIcon />}
+                    />
+                  </div>
                 </div>
+                <div className="mt-3 mb-3 text-base text-left text-primary-lighter">
+                  When providing liquidity to the liquidity bins of the Chromatic protocol,
+                  providers are rewarded by minting CLB tokens. CLB tokens follow the ERC-1155
+                  standard and have one token contract per market, with each bin having its own
+                  unique token ID.{' '}
+                  <Outlink outLink="https://chromatic-protocol.gitbook.io/docs/tokens/clb-token-erc-1155" />
+                </div>
+              </article>
+              <div className="mt-10">
+                <Link to={'/trade'}>
+                  <Button
+                    label={
+                      isValid(selectedMarket)
+                        ? `Trade on ${selectedMarket.description} Pool`
+                        : 'Market loading'
+                    }
+                    iconRight={<ChevronRightIcon />}
+                  />
+                </Link>
               </div>
-              <div className="mt-3 mb-3 text-base text-left text-primary-lighter">
-                When providing liquidity to the liquidity bins of the Chromatic protocol, providers
-                are rewarded by minting CLB tokens. CLB tokens follow the ERC-1155 standard and have
-                one token contract per market, with each bin having its own unique token ID.{' '}
-                <Outlink outLink="https://chromatic-protocol.gitbook.io/docs/tokens/clb-token-erc-1155" />
-              </div>
-            </article>
-            <div className="mt-10">
-              <Link to={'/trade'}>
-                <Button
-                  label={
-                    isValid(selectedMarket)
-                      ? `Trade on ${selectedMarket.description} Pool`
-                      : 'Market loading'
-                  }
-                  iconRight={<ChevronRightIcon />}
-                />
-              </Link>
+            </div>
+            <div className="w-2/5 max-w-[500px] min-w-[480px]">
+              <PoolProgress />
             </div>
           </div>
-          <div className="w-2/5 max-w-[500px] min-w-[480px]">
-            <PoolProgress />
-          </div>
-        </div>
-      </section>
+        </section>
+      </div>
       <Footer />
       <Toast />
       <ChainModal />
