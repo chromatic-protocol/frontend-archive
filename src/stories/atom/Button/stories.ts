db1f59f77d9bbc4509eb5d96af71e265034a33f2
dia.nn <dia.nn@quarkonix.com>
2023-08-11 17:37:21 +0900

    style: update `Button` for dark

--- src/stories/atom/Button/stories.ts
+++ src/stories/atom/Button/stories.ts
@@ -1,11 +1,11 @@
-import type { Meta, StoryObj } from "@storybook/react";
-import { Button } from ".";
+import type { Meta, StoryObj } from '@storybook/react';
+import { Button } from '.';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta = {
-  title: "Atom/Button",
+  title: 'Atom/Button',
   component: Button,
-  tags: ["autodocs"],
+  tags: ['autodocs'],
   argTypes: {
     // backgroundColor: { control: "color" },
   },
@@ -17,21 +17,21 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Default: Story = {
   args: {
-    css: "default",
-    label: "Button",
-  },
-};
-
-export const Active: Story = {
-  args: {
-    css: "active",
-    label: "Button",
+    css: 'default',
+    label: 'Button',
   },
 };
 
 export const Gray: Story = {
   args: {
-    css: "gray",
-    label: "Button",
+    css: 'gray',
+    label: 'Button',
+  },
+};
+
+export const Active: Story = {
+  args: {
+    css: 'active',
+    label: 'Button',
   },
 };
