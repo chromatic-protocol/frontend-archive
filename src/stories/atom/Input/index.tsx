1ebc0f9cb3ea0c9019eeb6579f7792d89011870d
austin-builds <austin.31@quarkonix.com>
2023-08-11 19:28:11 +0900

    fix: prevent border error before input

--- src/stories/atom/Input/index.tsx
+++ src/stories/atom/Input/index.tsx
@@ -1,11 +1,8 @@
-import './style.css';
-
-import { ChangeEvent, useEffect, useState } from 'react';
-
-import { Avatar } from '~/stories/atom/Avatar';
-
 import { isNil, isNotNil } from 'ramda';
+import { ChangeEvent, useState } from 'react';
+import { Avatar } from '~/stories/atom/Avatar';
 import { withComma } from '~/utils/number';
+import './style.css';
 
 interface InputProps {
   label?: string;
@@ -48,11 +45,6 @@ export const Input = (props: InputProps) => {
   const [tempValue, setTempValue] = useState(value);
   const [isInternalChange, setIsInternalChange] = useState(false);
 
-  useEffect(() => {
-    if (!isInternalChange) setTempValue(value);
-    return setIsInternalChange(false);
-  }, [value]);
-
   function isOverMax(newValue?: string | number) {
     return newValue === undefined || isNil(max) || +newValue > max;
   }
@@ -67,6 +59,7 @@ export const Input = (props: InputProps) => {
 
   function handleChange(event: ChangeEvent<HTMLInputElement>) {
     event.preventDefault();
+    setIsInternalChange(true);
     const newValue = trimLeadingZero(event.target.value)
       .replaceAll(',', '')
       .replace(/[^0-9.]/g, '');
@@ -79,8 +72,6 @@ export const Input = (props: InputProps) => {
       setTempValue(newValue);
       return;
     }
-
-    setIsInternalChange(true);
 
     if (!autoCorrect) {
       setTempValue(newValue);
@@ -101,6 +92,9 @@ export const Input = (props: InputProps) => {
   }
 
   function handleBlur() {
+    if (tempValue === undefined || String(tempValue).length === 0) {
+      setIsInternalChange(false);
+    }
     if (autoCorrect && isNotNil(min) && isUnderMin(tempValue)) {
       setTempValue(value);
       return;
@@ -115,7 +109,7 @@ export const Input = (props: InputProps) => {
     <>
       <div
         className={`inline-flex gap-1 items-center input input-${size} input-${css} ${className} ${
-          error ? 'error' : ''
+          isInternalChange && error ? 'error' : ''
         } ${disabled ? 'disabled' : ''}`}
       >
         {assetSrc ? <Avatar src={assetSrc} size="sm" /> : null}
