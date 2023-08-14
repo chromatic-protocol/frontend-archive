7ff0676f01927777fede9fb7746a67a3c9d56040
dia.nn <dia.nn@quarkonix.com>
2023-08-14 19:00:19 +0900

    feat: `AddressCopyButton` style, tooltip

--- src/stories/atom/AddressCopyButton/index.tsx
+++ src/stories/atom/AddressCopyButton/index.tsx
@@ -1,6 +1,7 @@
 import { Square2StackIcon } from '@heroicons/react/24/outline';
 import { MouseEventHandler } from 'react';
 import { Button } from '../Button';
+import { TooltipGuide } from '../TooltipGuide';
 import Skeleton from 'react-loading-skeleton';
 
 interface AddressCopyButtonProps {
@@ -14,7 +15,7 @@ export const AddressCopyButton = (props: AddressCopyButtonProps) => {
 
   return (
     <>
-      <div className="flex items-center justify-between flex-auto border border-collapse rounded-full max-w-[220px] bg-paper text-primary">
+      <div className="flex items-center justify-between flex-auto border dark:border-transparent border-collapse rounded-full max-w-[220px] bg-paper dark:bg-paper-light text-primary">
         <p className="w-[calc(100%-40px)] px-4 overflow-hidden min-w-[80px]">
           {address ? <>{address}</> : <Skeleton width={60} />}
         </p>
@@ -22,11 +23,12 @@ export const AddressCopyButton = (props: AddressCopyButtonProps) => {
           label="Copy Address"
           css="light"
           size="lg"
-          className="m-[-1px]"
+          className="m-[-1px] tooltip-copy-address"
           iconOnly={<Square2StackIcon />}
           onClick={onClick}
         />
       </div>
+      <TooltipGuide tipOnly tip="Copy" label="copy-address" />
     </>
   );
 };
