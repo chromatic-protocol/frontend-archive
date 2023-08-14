b425d19180127196f128ba4201284d77e896f9be
austin-builds <austin.31@quarkonix.com>
2023-08-14 10:11:22 +0900

    fix: revert component

--- src/stories/atom/Input/index.tsx
+++ src/stories/atom/Input/index.tsx
@@ -1,5 +1,5 @@
 import { isNil, isNotNil } from 'ramda';
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import { Avatar } from '~/stories/atom/Avatar';
 import { withComma } from '~/utils/number';
 import './style.css';
@@ -45,6 +45,11 @@ export const Input = (props: InputProps) => {
   const [tempValue, setTempValue] = useState(value);
   const [isInternalChange, setIsInternalChange] = useState(false);
 
+  useEffect(() => {
+    if (!isInternalChange) setTempValue(value);
+    return setIsInternalChange(false);
+  }, [value]);
+
   function isOverMax(newValue?: string | number) {
     return newValue === undefined || isNil(max) || +newValue > max;
   }
@@ -59,7 +64,6 @@ export const Input = (props: InputProps) => {
 
   function handleChange(event: ChangeEvent<HTMLInputElement>) {
     event.preventDefault();
-    setIsInternalChange(true);
     const newValue = trimLeadingZero(event.target.value)
       .replaceAll(',', '')
       .replace(/[^0-9.]/g, '');
@@ -72,6 +76,8 @@ export const Input = (props: InputProps) => {
       setTempValue(newValue);
       return;
     }
+
+    setIsInternalChange(true);
 
     if (!autoCorrect) {
       setTempValue(newValue);
@@ -92,9 +98,6 @@ export const Input = (props: InputProps) => {
   }
 
   function handleBlur() {
-    if (tempValue === undefined || String(tempValue).length === 0) {
-      setIsInternalChange(false);
-    }
     if (autoCorrect && isNotNil(min) && isUnderMin(tempValue)) {
       setTempValue(value);
       return;
@@ -109,7 +112,7 @@ export const Input = (props: InputProps) => {
     <>
       <div
         className={`inline-flex gap-1 items-center input input-${size} input-${css} ${className} ${
-          isInternalChange && error ? 'error' : ''
+          error ? 'error' : ''
         } ${disabled ? 'disabled' : ''}`}
       >
         {assetSrc ? <Avatar src={assetSrc} size="sm" /> : null}
