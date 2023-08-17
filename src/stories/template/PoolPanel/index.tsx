ed5a56f26dee297f9fdf2d5633487a20058612cf
dia.nn <dia.nn@quarkonix.com>
2023-08-17 11:15:14 +0900

    style: PoolPanel > remove > box-top

--- src/stories/template/PoolPanel/index.tsx
+++ src/stories/template/PoolPanel/index.tsx
@@ -422,7 +422,7 @@ export const PoolPanel = (props: PoolPanelProps) => {
             <Tab.Panel className="w-full">
               <section className="flex items-stretch gap-5">
                 {/* liquidity value */}
-                <article className="items-start justify-around box-top xl:flex-row xl:items-center xl:justify-between py-7">
+                <article className="flex-col items-start justify-around box-top lg:flex-row lg:items-center lg:justify-between py-7">
                   <div>
                     <div className="flex font-semibold text-left text-primary-lighter">
                       Total Liquidity Value
@@ -450,7 +450,7 @@ export const PoolPanel = (props: PoolPanelProps) => {
                   </h4>
                 </article>
                 {/* info */}
-                <article className="justify-between gap-3 py-4 text-left box-top xl:gap-2 xl:py-7">
+                <article className="flex-col justify-between gap-3 py-4 text-left box-top xl:gap-2 xl:py-7">
                   <div className="flex flex-col justify-between gap-1 xl:text-right xl:flex-row">
                     <div className="flex items-center font-medium text-left text-primary-lighter">
                       LP Bins
