10f541241ac39138eb18fa306f0270aafddf4bde
dia.nn <dia.nn@quarkonix.com>
2023-08-23 11:19:02 +0900

    style: set config font-weight as default

--- tailwind.config.js
+++ tailwind.config.js
@@ -93,12 +93,12 @@ export const theme = {
       '4xl': ['24px', 'normal'],
     },
     fontWeight: {
-      light: 200,
-      normal: 300,
-      medium: 400,
-      semibold: 500,
-      bold: 600,
-      extrabold: 700,
+      // light: 200,
+      // normal: 300,
+      // medium: 400,
+      // semibold: 500,
+      // bold: 600,
+      // extrabold: 700,
     },
     animation: {
       'spin-slow': 'spin 3s linear infinite',
