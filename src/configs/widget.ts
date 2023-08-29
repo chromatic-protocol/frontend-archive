4ec7c31ef6478b6f5696f0783b846df41b5d90f0
austin-builds <austin.31@quarkonix.com>
2023-08-29 17:31:41 +0900

    feat: market symbols for develop

---
+++ src/configs/widget.ts
@@ -0,0 +1,7 @@
+// FIXME: Temporary used symbols for showing chart.
+export const MARKET_SYMBOLS = [
+  'COINBASE:BTCUSD',
+  'COINBASE:ETHUSD',
+  'COINBASE:MATICUSD',
+  'COINBASE:ARBUSD',
+] as const;
