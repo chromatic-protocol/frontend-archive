1dc28b191b2dd85b391359a45ab42635b94576f2
dia.nn <dia.nn@quarkonix.com>
2023-08-30 19:04:40 +0900

    feat: add `BookmarkButton` to `MarketSelectV2`

--- src/stories/atom/BookmarkButton/index.tsx
+++ src/stories/atom/BookmarkButton/index.tsx
@@ -1,24 +1,29 @@
 import { useState } from 'react';
 import { StarIcon } from '@heroicons/react/24/outline';
-import '../Button/style.css';
 
 interface BookmarkButtonProps {
   initialBookmarkState?: boolean;
+  size?: 'base' | 'lg' | 'sm';
+  className?: string;
 }
 
 export const BookmarkButton = (props: BookmarkButtonProps) => {
-  const { initialBookmarkState = false } = props;
+  const { initialBookmarkState = false, size = 'base', className } = props;
   const [isBookmarked, setIsBookmarked] = useState(initialBookmarkState);
   const handleBookmarkToggle = () => {
     setIsBookmarked(!isBookmarked);
   };
 
   return (
-    <button className="btn-bookmark" onClick={handleBookmarkToggle} title="bookmark toggle">
+    <button
+      className={`btn-bookmark ${className}`}
+      onClick={handleBookmarkToggle}
+      title="bookmark toggle"
+    >
       <StarIcon
-        className={`w-6 h-6 stroke-primary-lighter ${
-          isBookmarked ? 'fill-primary stroke-primary' : ''
-        }`}
+        className={`stroke-primary-lighter ${isBookmarked ? 'fill-primary stroke-primary' : ''}    
+        ${size === 'base' ? 'w-4 h-4' : size === 'lg' ? 'w-6 h-6' : size === 'sm' ? '' : ''}
+          `}
       />
     </button>
   );
--- src/stories/molecule/MarketSelectV2/index.tsx
+++ src/stories/molecule/MarketSelectV2/index.tsx
@@ -5,6 +5,7 @@ import { ChevronDownIcon, ChevronRightIcon } from '@heroicons/react/20/solid';
 import { Avatar } from '~/stories/atom/Avatar';
 import { SkeletonElement } from '~/stories/atom/SkeletonElement';
 import { TooltipGuide } from '~/stories/atom/TooltipGuide';
+import { BookmarkButton } from '~/stories/atom/BookmarkButton';
 
 import { useMarketSelectV2 } from './hooks';
 
@@ -23,64 +24,70 @@ export function MarketSelectV2() {
   return (
     <>
       <div className="relative MarketSelectV2 panel">
-        <Popover>
-          <Popover.Button className="flex items-center gap-3 ml-10">
-            <div className="pr-3 border-r">
-              <div className="flex items-center gap-1">
-                <SkeletonElement isLoading={isLoading} circle width={24} height={24} />
-                <SkeletonElement isLoading={isLoading} width={60} containerClassName="text-2xl">
-                  <Avatar label={tokenName} fontSize="2xl" gap="1" size="sm" />
-                </SkeletonElement>
+        <div className="flex items-center gap-3">
+          <BookmarkButton size="lg" />
+          <Popover>
+            <Popover.Button className="flex items-center gap-3">
+              <div className="pr-3 border-r">
+                <div className="flex items-center gap-1">
+                  <SkeletonElement isLoading={isLoading} circle width={24} height={24} />
+                  <SkeletonElement isLoading={isLoading} width={60} containerClassName="text-2xl">
+                    <Avatar label={tokenName} fontSize="2xl" gap="1" size="sm" />
+                  </SkeletonElement>
+                </div>
               </div>
-            </div>
-            <div>
-              <div className="flex items-center gap-1">
-                <SkeletonElement isLoading={isLoading} circle width={24} height={24} />
-                <SkeletonElement isLoading={isLoading} width={80} containerClassName="text-2xl">
-                  <Avatar label={marketDescription} fontSize="2xl" gap="1" size="sm" />
-                </SkeletonElement>
+              <div>
+                <div className="flex items-center gap-1">
+                  <SkeletonElement isLoading={isLoading} circle width={24} height={24} />
+                  <SkeletonElement isLoading={isLoading} width={80} containerClassName="text-2xl">
+                    <Avatar label={marketDescription} fontSize="2xl" gap="1" size="sm" />
+                  </SkeletonElement>
+                </div>
               </div>
-            </div>
-            <ChevronDownIcon
-              className="w-5 h-5 transition duration-150 ease-in-out"
-              aria-hidden="true"
-            />
-          </Popover.Button>
-          <Popover.Panel className="flex popover-panel">
-            <section className="flex w-full py-4 border-t">
-              <article className="flex flex-col pr-6 mr-6 border-r">
-                {tokens.map(({ key, isSelectedToken, onClickToken, name }) => (
-                  <button
-                    key={key}
-                    className={`flex items-center gap-2 px-4 py-2 ${
-                      isSelectedToken && 'text-inverted bg-primary rounded-lg' // the token selected
-                    }`}
-                    onClick={onClickToken}
-                    title={name}
-                  >
-                    <Avatar label={name} fontSize="lg" gap="2" size="sm" />
-                    {isSelectedToken && <ChevronRightIcon className="w-4" />}
-                  </button>
-                ))}
-              </article>
+              <ChevronDownIcon
+                className="w-5 h-5 transition duration-150 ease-in-out"
+                aria-hidden="true"
+              />
+            </Popover.Button>
+            <Popover.Panel className="flex popover-panel">
+              <section className="flex w-full py-4 border-t">
+                <article className="flex flex-col pr-6 mr-6 border-r">
+                  {tokens.map(({ key, isSelectedToken, onClickToken, name }) => (
+                    <button
+                      key={key}
+                      className={`flex items-center gap-2 px-4 py-2 ${
+                        isSelectedToken && 'border bg-paper-lighter rounded-lg' // the token selected
+                      }`}
+                      onClick={onClickToken}
+                      title={name}
+                    >
+                      <Avatar label={name} fontSize="lg" gap="2" size="sm" />
+                      {isSelectedToken && <ChevronRightIcon className="w-4" />}
+                    </button>
+                  ))}
+                </article>
 
-              <article className="flex flex-col flex-auto">
-                {markets.map(({ key, isSelectedMarket, onClickMarket, description, price }) => (
-                  <button
-                    key={key}
-                    className={`flex items-center justify-between gap-4 px-4 py-2 ${
-                      isSelectedMarket && 'text-inverted bg-primary rounded-lg'
-                    }`}
-                    onClick={onClickMarket}
-                  >
-                    <Avatar label={description} fontSize="lg" gap="2" size="sm" />
-                    <p>${price}</p>
-                  </button>
-                ))}
-              </article>
-            </section>
-          </Popover.Panel>
-        </Popover>
+                <article className="flex flex-col flex-auto">
+                  {markets.map(({ key, isSelectedMarket, onClickMarket, description, price }) => (
+                    <div className="relative flex items-center w-full">
+                      <BookmarkButton className="absolute left-0 ml-2" />
+                      <button
+                        key={key}
+                        className={`w-full flex items-center justify-between gap-4 pl-8 py-2 pr-2 ${
+                          isSelectedMarket && 'border bg-paper-lighter rounded-lg'
+                        }`}
+                        onClick={onClickMarket}
+                      >
+                        <Avatar label={description} fontSize="lg" gap="2" size="sm" />
+                        <span>${price}</span>
+                      </button>
+                    </div>
+                  ))}
+                </article>
+              </section>
+            </Popover.Panel>
+          </Popover>
+        </div>
         <div className="flex items-center gap-5 mr-10">
           <h2 className={`text-3xl ${priceClass}`}>
             <SkeletonElement isLoading={isLoading} width={80}>
