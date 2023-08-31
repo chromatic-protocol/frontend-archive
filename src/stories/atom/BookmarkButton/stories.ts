9b8db8592a0c2d984c497666e381e2a6447cdb9b
dia.nn <dia.nn@quarkonix.com>
2023-08-31 14:26:04 +0900

    fix: `BookmarkButton` stories

--- src/stories/atom/BookmarkButton/stories.ts
+++ src/stories/atom/BookmarkButton/stories.ts
@@ -16,7 +16,5 @@ type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Default: Story = {
-  args: {
-    label: 'BookmarkButton',
-  },
+  args: {},
 };
