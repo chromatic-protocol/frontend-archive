db90ea357d8a228e11cb3db8a12fbfd46399e840
austin-builds <austin.31@quarkonix.com>
2023-09-04 14:33:32 +0900

    fix: property naming `*-rule`

--- src/assets/icons/ArrowTriangleIcon.tsx
+++ src/assets/icons/ArrowTriangleIcon.tsx
@@ -4,9 +4,9 @@ export default function ArrowTriangleIcon(props: any) {
       <path className="fill-current" d="m9 5.375-3 3-3-3" />
       <path
         className="fill-current"
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M2.654 5.231A.375.375 0 0 1 3 5h6a.375.375 0 0 1 .265.64l-3 3a.375.375 0 0 1-.53 0l-3-3a.375.375 0 0 1-.081-.409Zm1.251.519L6 7.845 8.095 5.75h-4.19Z"
-        clip-rule="evenodd"
+        clipRule="evenodd"
       />
     </svg>
   );
