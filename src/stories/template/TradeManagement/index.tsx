871d79fd61c002c088fbdc4f883d86f7531803fc
dia.nn <dia.nn@quarkonix.com>
2023-09-05 18:13:17 +0900

    style: TradeManagement tabs, scrollable

--- src/stories/template/TradeManagement/index.tsx
+++ src/stories/template/TradeManagement/index.tsx
@@ -57,14 +57,16 @@ export const TradeManagement = () => {
         onResizeStop={handleResizeStop}
         className="panel"
       >
-        <div className="w-full h-full overflow-hidden tabs tabs-default">
+        <div className="w-full h-full tabs tabs-default tabs-left">
           <Tab.Group>
-            <div className="flex items-center w-full px-5">
-              <Tab.List>
-                <Tab className="">Position</Tab>
-                <Tab className="">History</Tab>
-                <Tab className="">Trades</Tab>
-              </Tab.List>
+            <div className="flex flex-col w-full h-full">
+              <div className="border-b">
+                <Tab.List className="tabs-list">
+                  <Tab>Position</Tab>
+                  <Tab>History</Tab>
+                  <Tab>Trades</Tab>
+                </Tab.List>
+              </div>
               {/* <div className="flex items-center gap-5 mt-4 ml-auto">
                 <p className="pr-5 text-sm border-r text-primary-lighter">
                   Last Oracle Update: {lastOracle.hours}h {lastOracle.minutes}m {lastOracle.seconds}
@@ -77,83 +79,85 @@ export const TradeManagement = () => {
                   </SkeletonElement>
                 </p>
               </div> */}
-            </div>
-            <Tab.Panels className="overflow-auto mt-7">
-              <Tab.Panel className="px-10 pb-10">
-                <article>
-                  {/* guide next round */}
-                  {isGuideVisible && (
-                    <div className="mb-3">
-                      <Guide
+              <Tab.Panels className="h-[calc(100%-40px)]">
+                <Tab.Panel className="h-full">
+                  <article className="h-full">
+                    {/* guide next round */}
+                    {isGuideVisible && (
+                      <div className="">
+                        {/* <Guide
                         title="Next Oracle Round"
                         // paragraph 내 퍼센트 값은 마켓마다 다르게 불러오는 값입니다.
                         paragraph={`Waiting for the next oracle round. The next oracle round is updated whenever the Chainlink price moves by ${PERCENTAGE}% or more, and it is updated at least once a day.`}
                         outLink="https://chromatic-protocol.gitbook.io/docs/trade/settlement#next-oracle-round-mechanism-in-settlement"
                         outLinkAbout="Next Oracle Round"
                         flex
-                      />
-                    </div>
-                  )}
-                  <div className="flex flex-col gap-3">
-                    {isPositionsEmpty ? (
-                      <p className="mt-10 text-center text-primary/20">You have no position yet.</p>
-                    ) : (
-                      <div className="list position">
-                        <div className="thead">
-                          <div className="tr">
-                            <div className="td">Position</div>
-                            <div className="td">Entry Price</div>
-                            <div className="td">Contract Qty</div>
-                            <div className="td">Leverage</div>
-                            <div className="td">TP Price</div>
-                            <div className="td">SL Price</div>
-                            <div className="td">PnL</div>
-                            <div className="td">Close</div>
-                          </div>
-                        </div>
-                        <div className="tbody">
-                          {positionList.map((position) => (
-                            <PositionItemV2 key={position.id.toString()} position={position} />
-                          ))}
-                        </div>
+                      /> */}
                       </div>
                     )}
-                    <div>
-                      <TooltipGuide
-                        tipOnly
-                        label="opening-in-progress"
-                        // TODO: PERCENTAGE
-                        tip={`Waiting for the next oracle round to open the position. The next oracle round is updated whenever the Chainlink price moves by ${PERCENTAGE}% or more, and it is updated at least once a day.`}
-                        outLink="https://chromatic-protocol.gitbook.io/docs/trade/settlement#next-oracle-round-mechanism-in-settlement"
-                        outLinkAbout="Next Oracle Round"
-                      />
-                      <TooltipGuide
-                        tipOnly
-                        label="opening-completed"
-                        tip="The opening process has been completed. Now the position is in live status."
-                        outLink="https://chromatic-protocol.gitbook.io/docs/trade/settlement#next-oracle-round-mechanism-in-settlement"
-                        outLinkAbout="Next Oracle Round"
-                      />
-                      <TooltipGuide
-                        tipOnly
-                        label="closing-in-progress"
-                        // TODO: PERCENTAGE
-                        tip={`Waiting for the next oracle round to close the position. The next oracle round is updated whenever the Chainlink price moves by ${PERCENTAGE}% or more, and it is updated at least once a day.`}
-                        outLink="https://chromatic-protocol.gitbook.io/docs/trade/settlement#next-oracle-round-mechanism-in-settlement"
-                        outLinkAbout="Next Oracle Round"
-                      />
-                      <TooltipGuide
-                        tipOnly
-                        label="closing-completed"
-                        tip="The closing process has been completed. You can claim the assets and transfer them to your account."
-                        outLink="https://chromatic-protocol.gitbook.io/docs/trade/settlement#next-oracle-round-mechanism-in-settlement"
-                        outLinkAbout="Next Oracle Round"
-                      />
+                    <div className="h-full">
+                      {isPositionsEmpty ? (
+                        <p className="mt-10 text-center text-primary/20">
+                          You have no position yet.
+                        </p>
+                      ) : (
+                        <div className="h-full overflow-hidden list position">
+                          <div className="thead">
+                            <div className="tr">
+                              <div className="td">Position</div>
+                              <div className="td">Entry Price</div>
+                              <div className="td">Contract Qty</div>
+                              <div className="td">Leverage</div>
+                              <div className="td">TP Price</div>
+                              <div className="td">SL Price</div>
+                              <div className="td">PnL</div>
+                              <div className="td">Close</div>
+                            </div>
+                          </div>
+                          <div className="tbody h-[calc(100%-32px)]">
+                            {positionList.map((position) => (
+                              <PositionItemV2 key={position.id.toString()} position={position} />
+                            ))}
+                          </div>
+                        </div>
+                      )}
+                      <div>
+                        <TooltipGuide
+                          tipOnly
+                          label="opening-in-progress"
+                          // TODO: PERCENTAGE
+                          tip={`Waiting for the next oracle round to open the position. The next oracle round is updated whenever the Chainlink price moves by ${PERCENTAGE}% or more, and it is updated at least once a day.`}
+                          outLink="https://chromatic-protocol.gitbook.io/docs/trade/settlement#next-oracle-round-mechanism-in-settlement"
+                          outLinkAbout="Next Oracle Round"
+                        />
+                        <TooltipGuide
+                          tipOnly
+                          label="opening-completed"
+                          tip="The opening process has been completed. Now the position is in live status."
+                          outLink="https://chromatic-protocol.gitbook.io/docs/trade/settlement#next-oracle-round-mechanism-in-settlement"
+                          outLinkAbout="Next Oracle Round"
+                        />
+                        <TooltipGuide
+                          tipOnly
+                          label="closing-in-progress"
+                          // TODO: PERCENTAGE
+                          tip={`Waiting for the next oracle round to close the position. The next oracle round is updated whenever the Chainlink price moves by ${PERCENTAGE}% or more, and it is updated at least once a day.`}
+                          outLink="https://chromatic-protocol.gitbook.io/docs/trade/settlement#next-oracle-round-mechanism-in-settlement"
+                          outLinkAbout="Next Oracle Round"
+                        />
+                        <TooltipGuide
+                          tipOnly
+                          label="closing-completed"
+                          tip="The closing process has been completed. You can claim the assets and transfer them to your account."
+                          outLink="https://chromatic-protocol.gitbook.io/docs/trade/settlement#next-oracle-round-mechanism-in-settlement"
+                          outLinkAbout="Next Oracle Round"
+                        />
+                      </div>
                     </div>
-                  </div>
-                </article>
-              </Tab.Panel>
-            </Tab.Panels>
+                  </article>
+                </Tab.Panel>
+              </Tab.Panels>
+            </div>
           </Tab.Group>
         </div>
       </Resizable>
