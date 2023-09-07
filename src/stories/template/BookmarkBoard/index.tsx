cc4f9e02d437e8ee27e307ef13ccaaf6e708aa5c
austin-builds <austin.31@quarkonix.com>
2023-09-07 15:20:08 +0900

    feat: show bookmarks on `BookmarkBoard`

--- src/stories/template/BookmarkBoard/index.tsx
+++ src/stories/template/BookmarkBoard/index.tsx
@@ -1,31 +1,37 @@
-import '~/stories/atom/Tabs/style.css';
-import './style.css';
-import { Button } from '~/stories/atom/Button';
 import { StarIcon } from '@heroicons/react/20/solid';
-
-const BookmarkBoardItems = [
-  { name: 'USDC-ETH/USD', rate: 0.1212 },
-  { name: 'USDC-ETH/USD', rate: 0.1212 },
-  { name: 'USDC-ETH/USD', rate: -0.1212 },
-];
+import '~/stories/atom/Tabs/style.css';
+import { useBookmarkBoard } from './hooks';
+import './style.css';
 
 export interface BookmarkBoardProps {}
 
 export const BookmarkBoard = (props: BookmarkBoardProps) => {
+  const {
+    bookmarks = [],
+    bookmarkPrices,
+    bookmarkClasses,
+    isBookmarkLoading,
+    onBookmarkClick,
+  } = useBookmarkBoard();
   return (
     <div className="mb-2 BookmarkBoard">
       <div className="flex items-stretch h-6">
         <StarIcon className="w-4 mr-3" />
         <div className="flex gap-5">
-          {BookmarkBoardItems.map((item, index) => (
-            <a key={index} href="" className="item-bookmark">
-              <span>{item.name}</span>
-              <span
-                className={
-                  item.rate > 0 ? 'text-price-higher' : item.rate < 0 ? 'text-price-lower' : ''
-                }
-              >
-                {item.rate}%
+          {bookmarks.map((bookmark, index) => (
+            // eslint-disable-next-line jsx-a11y/anchor-is-valid
+            <a
+              key={bookmark.marketAddress}
+              href="#"
+              className="item-bookmark"
+              onClick={(event) => {
+                event.preventDefault();
+                onBookmarkClick(bookmark);
+              }}
+            >
+              <span>{bookmark.name}</span>
+              <span className={bookmarkClasses?.[bookmark.marketAddress]}>
+                ${bookmarkPrices?.[bookmark.marketAddress]}
               </span>
             </a>
           ))}
