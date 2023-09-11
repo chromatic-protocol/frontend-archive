5bbc36335de72fe1050162faea19cfbfa6d59721
austin-builds <austin.31@quarkonix.com>
2023-09-11 16:02:53 +0900

    feat: hooks to return trade history

--- src/stories/template/TradeManagement/hooks.ts
+++ src/stories/template/TradeManagement/hooks.ts
@@ -1,6 +1,7 @@
 import { isNil, isNotNil } from 'ramda';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import { toast } from 'react-toastify';
+import { ORACLE_PROVIDER_DECIMALS, PERCENT_DECIMALS, PNL_RATE_DECIMALS } from '~/configs/decimals';
 
 import { useLastOracle } from '~/hooks/useLastOracle';
 import { useMarket } from '~/hooks/useMarket';
@@ -11,12 +12,22 @@ import { useTradeHistory } from '~/hooks/useTradeHistory';
 import { TRADE_EVENT } from '~/typings/events';
 import { POSITION_STATUS } from '~/typings/position';
 
-import { formatDecimals } from '~/utils/number';
+import { abs, divPreserved, formatDecimals } from '~/utils/number';
+
+const dateFormat = new Intl.DateTimeFormat('en-US', {
+  second: '2-digit',
+  minute: '2-digit',
+  hour: '2-digit',
+  month: 'long',
+  day: 'numeric',
+  year: 'numeric',
+  hour12: false,
+}).format;
 
 export function useTradeManagement() {
   const { currentMarket } = useMarket();
   const { positions, isLoading } = usePositions();
-  const { history } = useTradeHistory();
+  const { history, isLoading: isHistoryLoading } = useTradeHistory();
   const previousOracle = usePrevious(currentMarket?.oracleValue.version);
   const openingPositionSize = usePrevious(
     positions?.filter((position) => position.status === POSITION_STATUS.OPENING).length ?? 0
@@ -65,6 +76,74 @@ export function useTradeManagement() {
   const isPositionsEmpty = isNil(positions) || positions.length === 0;
   const positionList = positions || [];
 
+  const historyList = useMemo(() => {
+    if (isNil(history)) {
+      return;
+    }
+    return history
+      .filter((historyValue) => historyValue.isClaimed)
+      .map((historyValue) => {
+        return {
+          token: historyValue.token,
+          market: historyValue.market,
+          positionId: historyValue.positionId,
+          direction: historyValue.direction,
+          collateral:
+            formatDecimals(historyValue.collateral, historyValue.token.decimals) +
+            ' ' +
+            historyValue.token.name,
+          qty:
+            formatDecimals(abs(historyValue.qty), historyValue.token.decimals) +
+            ' ' +
+            historyValue.token.name,
+          entryPrice:
+            '$' + formatDecimals(historyValue.entryPrice, ORACLE_PROVIDER_DECIMALS, 2, true),
+          leverage:
+            formatDecimals(historyValue.leverage, historyValue.token.decimals, 2, true) + 'x',
+          pnl:
+            formatDecimals(historyValue.pnl, historyValue.token.decimals) +
+            ' ' +
+            historyValue.token.name,
+          pnlRate:
+            formatDecimals(
+              divPreserved(
+                historyValue.pnl,
+                historyValue.collateral,
+                PNL_RATE_DECIMALS + PERCENT_DECIMALS
+              ),
+              PNL_RATE_DECIMALS,
+              2,
+              true
+            ) + '%',
+          entryTime: dateFormat(new Date(Number(historyValue.entryTimestamp) * 1000)),
+          closeTime: dateFormat(new Date(Number(historyValue.closeTimestamp) * 1000)),
+        };
+      });
+  }, [history]);
+
+  const tradeList = useMemo(() => {
+    return history?.map((historyValue) => {
+      return {
+        token: historyValue.token,
+        market: historyValue.market,
+        positionId: historyValue.positionId,
+        direction: historyValue.direction,
+        collateral:
+          formatDecimals(historyValue.collateral, historyValue.token.decimals) +
+          ' ' +
+          historyValue.token.name,
+        qty:
+          formatDecimals(abs(historyValue.qty), historyValue.token.decimals) +
+          ' ' +
+          historyValue.token.name,
+        entryPrice:
+          '$' + formatDecimals(historyValue.entryPrice, ORACLE_PROVIDER_DECIMALS, 2, true),
+        leverage: formatDecimals(historyValue.leverage, historyValue.token.decimals, 2, true) + 'x',
+        entryTime: dateFormat(new Date(Number(historyValue.entryTimestamp) * 1000)),
+      };
+    });
+  }, [history]);
+
   return {
     openButtonRef,
     popoverRef,
@@ -78,5 +157,9 @@ export function useTradeManagement() {
 
     isPositionsEmpty,
     positionList,
+
+    isHistoryLoading,
+    historyList,
+    tradeList,
   };
 }
