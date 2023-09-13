f8a209e7b9d738b1fdad63e3f8a31ab0b5f7fafa
dia.nn <dia.nn@quarkonix.com>
2023-09-13 16:30:13 +0900

    feat: Add `PoolDetail`

--- src/pages/poolV2/index.tsx
+++ src/pages/poolV2/index.tsx
@@ -1,37 +1,24 @@
-import { ChevronRightIcon } from '@heroicons/react/24/outline';
-import { isNotNil } from 'ramda';
-import { Link } from 'react-router-dom';
-import OutlinkIcon from '~/assets/icons/OutlinkIcon';
 import PlusIcon from '~/assets/icons/PlusIcon';
-import { useBlockExplorer } from '~/hooks/useBlockExplorer';
-import { useMarket } from '~/hooks/useMarket';
 import { useMarketLocal } from '~/hooks/useMarketLocal';
 import { useTokenLocal } from '~/hooks/useTokenLocal';
-import { AddressCopyButton } from '~/stories/atom/AddressCopyButton';
 import { Button } from '~/stories/atom/Button';
-import { Outlink } from '~/stories/atom/Outlink';
 import { Toast } from '~/stories/atom/Toast';
 import { ChainModal } from '~/stories/container/ChainModal';
-import { PoolProgress } from '~/stories/molecule/PoolProgress';
 import { Footer } from '~/stories/template/Footer';
 import { Header } from '~/stories/template/Header';
 import { MainBarV2 } from '~/stories/template/MainBarV2';
-import { trimAddress } from '~/utils/address';
-import { copyText } from '~/utils/clipboard';
 import { Tag } from '~/stories/atom/Tag';
 import { Avatar } from '~/stories/atom/Avatar';
 import { PoolPerformance } from '~/stories/template/PoolPerformance';
 import { PoolStat } from '~/stories/template/PoolStat';
 import { PoolMenu } from '~/stories/template/PoolMenu';
 import { PoolPanelV2 } from '~/stories/template/PoolPanelV2';
+import { PoolDetail } from '~/stories/template/PoolDetail';
 import './style.css';
 
 const PoolV2 = () => {
-  const { currentMarket: selectedMarket, clbTokenAddress } = useMarket();
   useTokenLocal();
   useMarketLocal();
-
-  const blockExplorer = useBlockExplorer();
 
   return (
     <div className="flex flex-col min-h-[100vh] w-full">
@@ -71,61 +58,13 @@ const PoolV2 = () => {
             <div className="flex gap-3">
               <div className="flex-auto overflow-hidden">
                 <PoolPanelV2 />
-                {/* ㄴ <PoolProgressV2 /> */}
                 {/* <PoolAnalytics /> */}
-
-                {/* bottom */}
-                {/* <article className="p-5 mx-auto mt-3 border shadow-lg dark:border-transparent dark:shadow-none bg-paper rounded-2xl">
-              <div className="flex items-center justify-between w-full gap-1">
-                <h4 className="font-bold">Token(ERC-1155) Contract Address</h4>
-                <div className="flex gap-2">
-                  <AddressCopyButton
-                    address={clbTokenAddress && trimAddress(clbTokenAddress, 6, 6)}
-                    onClick={() => {
-                      if (clbTokenAddress) {
-                        copyText(clbTokenAddress);
-                      }
-                    }}
-                  />
-                  <Button
-                    href={
-                      clbTokenAddress && blockExplorer
-                        ? `${blockExplorer}/token/${clbTokenAddress}`
-                        : undefined
-                    }
-                    label="view scanner"
-                    css="light"
-                    size="lg"
-                    iconOnly={<OutlinkIcon />}
-                  />
-                </div>
-              </div>
-              <div className="mt-3 mb-3 text-base text-left text-primary-lighter">
-                When providing liquidity to the liquidity bins of the Chromatic protocol, providers
-                are rewarded by minting CLB tokens. CLB tokens follow the ERC-1155 standard and have
-                one token contract per market, with each bin having its own unique token ID.{' '}
-                <Outlink outLink="https://chromatic-protocol.gitbook.io/docs/tokens/clb-token-erc-1155" />
-              </div>
-            </article> */}
-                {/* <div className="mt-10">
-              <Link to={'/trade'}>
-                <Button
-                  css="light"
-                  className="dark:!bg-paper-light dark:hover:!bg-gray-light"
-                  label={
-                    isNotNil(selectedMarket)
-                      ? `Trade on ${selectedMarket.description} Pool`
-                      : 'Market loading'
-                  }
-                  iconRight={<ChevronRightIcon />}
-                />
-              </Link>
-            </div> */}
               </div>
               <div className="flex-none w-[420px] flex flex-col gap-3">
                 {/* <PoolBalance /> */}
                 <PoolStat />
                 <PoolPerformance />
+                <PoolDetail />
               </div>
             </div>
           </div>
---
+++ src/stories/template/PoolDetail/index.tsx
@@ -0,0 +1,72 @@
+import { AddressCopyButton } from '~/stories/atom/AddressCopyButton';
+import { Button } from '~/stories/atom/Button';
+import { Outlink } from '~/stories/atom/Outlink';
+import OutlinkIcon from '~/assets/icons/OutlinkIcon';
+import ArrowTriangleIcon from '~/assets/icons/ArrowTriangleIcon';
+
+import { useBlockExplorer } from '~/hooks/useBlockExplorer';
+import { copyText } from '~/utils/clipboard';
+import { useMarket } from '~/hooks/useMarket';
+import { trimAddress } from '~/utils/address';
+import { isNotNil } from 'ramda';
+
+export interface PoolDetailProps {}
+
+export const PoolDetail = (props: PoolDetailProps) => {
+  const { currentMarket: selectedMarket, clbTokenAddress } = useMarket();
+  const blockExplorer = useBlockExplorer();
+
+  return (
+    <div className="p-5 PoolDetail panel">
+      <div className="flex items-center justify-between w-full gap-3">
+        <div className="text-xl text-left">
+          <h4>CLP-ETH-BTC/USD</h4>
+          {/* todo: change text-color for each risk - high / mid / low */}
+          <h4 className={`text-risk-high`}>Junior Pool</h4>
+        </div>
+        <div className="flex gap-2">
+          <AddressCopyButton
+            address={clbTokenAddress && trimAddress(clbTokenAddress, 6, 6)}
+            onClick={() => {
+              if (clbTokenAddress) {
+                copyText(clbTokenAddress);
+              }
+            }}
+            className="min-w-[200px]"
+          />
+          {/* <Button
+            href={
+              clbTokenAddress && blockExplorer
+                ? `${blockExplorer}/token/${clbTokenAddress}`
+                : undefined
+            }
+            label="view scanner"
+            css="light"
+            size="lg"
+            iconOnly={<OutlinkIcon />}
+          /> */}
+        </div>
+      </div>
+      <div className="pt-3 mt-3 text-left border-t">
+        <div className="text-base text-primary-light">
+          When providing liquidity to the liquidity bins of the Chromatic protocol, providers are
+          rewarded by minting CLB tokens. CLB tokens follow the ERC-1155 standard and have one token
+          contract per market, with each bin having its own unique token ID.{' '}
+          {/* <Outlink outLink="https://chromatic-protocol.gitbook.io/docs/tokens/clb-token-erc-1155" /> */}
+        </div>
+        <div className="mt-5">
+          <Button
+            to="/trade"
+            css="underlined"
+            label={
+              isNotNil(selectedMarket)
+                ? `Trade on ${selectedMarket.description} Pool`
+                : 'Market loading'
+            }
+            iconRight={<ArrowTriangleIcon className="-rotate-90" />}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
