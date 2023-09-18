b262b9df5ea33b61b06dfc81e1cf3ffad4e979ba
dia.nn <dia.nn@quarkonix.com>
2023-09-18 13:23:22 +0900

    feat: tradePanelV2 wide ui

--- src/stories/template/TradePanelV2/index.tsx
+++ src/stories/template/TradePanelV2/index.tsx
@@ -1,5 +1,5 @@
+import { useState, useEffect } from 'react';
 import { Tab } from '@headlessui/react';
-import { useState } from 'react';
 import DecreaseIcon from '~/assets/icons/DecreaseIcon';
 import IncreaseIcon from '~/assets/icons/IncreaseIcon';
 import '~/stories/atom/Tabs/style.css';
@@ -15,30 +15,67 @@ export const TradePanelV2 = () => {
   const [selectedTab, setSelectedTab] = useState<POSITION_TAB>(POSITION_TAB.SHORT_TAB);
   const onSelectTab = (tab: number) => setSelectedTab(tab);
 
+  const [isWide, setIsWide] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const screenWidth = window.innerWidth;
+      setIsWide(screenWidth > 2000);
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <div className="TradePanelV2 panel">
-      <div className="w-full tabs tabs-default tabs-lg">
-        <Tab.Group selectedIndex={selectedTab} onChange={onSelectTab}>
-          <Tab.List className="flex w-full">
-            <Tab value="short" className="btn-tabs short">
-              <DecreaseIcon />
-              SHORT
-            </Tab>
-            <Tab value="long" className="btn-tabs long">
-              <IncreaseIcon />
-              LONG
-            </Tab>
-          </Tab.List>
-          <Tab.Panels className="flex flex-col items-center w-full">
-            <Tab.Panel className="w-full">
+      {isWide ? (
+        <div className="relative w-full">
+          <div className="flex">
+            <div className="border-r">
+              <div className="flex items-center justify-center h-12 gap-2 text-3xl text-price-lower">
+                <DecreaseIcon />
+                SHORT
+              </div>
               <TradeContentV2 direction="short" />
-            </Tab.Panel>
-            <Tab.Panel className="w-full">
+            </div>
+            <div className="">
+              <div className="flex items-center justify-center h-12 gap-2 text-3xl text-price-higher">
+                <IncreaseIcon />
+                LONG
+              </div>
               <TradeContentV2 direction="long" />
-            </Tab.Panel>
-          </Tab.Panels>
-        </Tab.Group>
-      </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="w-full tabs tabs-default tabs-lg">
+          <Tab.Group selectedIndex={selectedTab} onChange={onSelectTab}>
+            <Tab.List className="flex w-full">
+              <Tab value="short" className="btn-tabs short">
+                <DecreaseIcon />
+                SHORT
+              </Tab>
+              <Tab value="long" className="btn-tabs long">
+                <IncreaseIcon />
+                LONG
+              </Tab>
+            </Tab.List>
+            <Tab.Panels className="flex flex-col items-center w-full">
+              <Tab.Panel className="w-full">
+                <TradeContentV2 direction="short" />
+              </Tab.Panel>
+              <Tab.Panel className="w-full">
+                <TradeContentV2 direction="long" />
+              </Tab.Panel>
+            </Tab.Panels>
+          </Tab.Group>
+        </div>
+      )}
     </div>
   );
 };
