93bd9796825fe589849876a18096aaee8255105d
austin-builds <austin.31@quarkonix.com>
2023-09-19 19:31:11 +0900

    feat: needed token images

---
+++ src/constants/logo.ts
@@ -0,0 +1,9 @@
+import CHRM from '../assets/tokens/CHRM.png';
+
+export const logos = {
+  ETH: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png',
+  BTC: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1.png',
+  WETH: 'https://s2.coinmarketcap.com/static/img/coins/64x64/2396.png',
+  USDC: 'https://s2.coinmarketcap.com/static/img/coins/64x64/3408.png',
+  CHRM,
+} as Record<string, string>;
