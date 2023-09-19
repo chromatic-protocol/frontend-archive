4032da339cb233b956ae5ce621207d0d2dc15579
austin-builds <austin.31@quarkonix.com>
2023-09-19 19:37:17 +0900

    fix: add comments

--- src/constants/logo.ts
+++ src/constants/logo.ts
@@ -1,5 +1,6 @@
 import CHRM from '../assets/tokens/CHRM.png';
 
+// FIXME: temporary use
 export const logos = {
   ETH: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png',
   BTC: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1.png',
