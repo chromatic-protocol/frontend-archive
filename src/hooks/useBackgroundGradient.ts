0d5f8e489276081366f78397258b39c70823ac77
austin-builds <austin.31@quarkonix.com>
2023-09-25 13:23:56 +0900

    refactor: hooks to check gradient conditions

--- src/hooks/useBackgroundGradient.ts
+++ src/hooks/useBackgroundGradient.ts
@@ -1,37 +1,71 @@
-import { useEffect, useState } from 'react';
+import { isNil } from 'ramda';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { useMarket } from './useMarket';
+import { usePreviousOracle } from './usePreviousOracle';
 
-function useBackgroundGradient(initialBeforeCondition: boolean, initialAfterCondition: boolean) {
-  const [beforeCondition, setBeforeCondition] = useState(initialBeforeCondition);
-  const [afterCondition, setAfterCondition] = useState(initialAfterCondition);
+function useBackgroundGradient() {
+  const { currentMarket } = useMarket();
+  const { previousOracle } = usePreviousOracle({ market: currentMarket });
+  const backgroundRef = useRef<HTMLDivElement | null>(null);
 
-  const toggleBeforeCondition = () => {
-    setBeforeCondition(!beforeCondition);
+  const [beforeCondition, setBeforeCondition] = useState<boolean>();
+  const [afterCondition, setAfterCondition] = useState<boolean>();
+  const checkCondition = useCallback((): [boolean, boolean] => {
+    if (isNil(currentMarket) || isNil(previousOracle)) {
+      return [false, false];
+    }
+    const currentPrice = currentMarket?.oracleValue.price;
+    const isIncreasedNow = currentPrice - previousOracle.oracleBefore1Day.price > 0n;
+    if (isNil(previousOracle.oracleBefore2Days)) {
+      return [true, isIncreasedNow];
+    }
+    const isIncreasedBefore1Day =
+      previousOracle.oracleBefore1Day.price - previousOracle.oracleBefore2Days.price > 0n;
+    return [isIncreasedBefore1Day, isIncreasedNow];
+  }, [currentMarket, previousOracle]);
+
+  const onLoadBackgroundRef = (element: HTMLDivElement | null) => {
+    backgroundRef.current = element;
   };
 
-  const toggleAfterCondition = () => {
-    setAfterCondition(!afterCondition);
+  const toggleConditions = (beforeOrAfter: 'before' | 'after') => {
+    switch (beforeOrAfter) {
+      case 'before': {
+        setBeforeCondition((condition) => !condition);
+        break;
+      }
+      case 'after': {
+        setAfterCondition((condition) => !condition);
+        break;
+      }
+    }
   };
 
   useEffect(() => {
-    const test = document.getElementById('gradient');
-    if (!test) return;
+    const isIncreased = checkCondition();
+    setBeforeCondition(isIncreased[0]);
+    setAfterCondition(isIncreased[1]);
+  }, [checkCondition]);
 
-    if (test) {
-      if (beforeCondition && afterCondition) {
-        test.style.background = 'rgb(var(--color-price-higher-light) / 0.19)';
-      } else if (beforeCondition) {
-        test.style.background =
-          'linear-gradient(90deg, rgb(var(--color-price-higher-light) / 0.19) 0%, rgb(var(--color-price-lower-light) / 0.19) 100%)';
-      } else if (afterCondition) {
-        test.style.background =
-          'linear-gradient(90deg, rgb(var(--color-price-lower-light) / 0.19) 0%, rgb(var(--color-price-higher-light) / 0.19) 100%)';
-      } else {
-        test.style.background = 'rgb(var(--color-price-lower-light) / 0.19)';
-      }
+  useEffect(() => {
+    if (!backgroundRef.current) {
+      return;
+    }
+
+    if (beforeCondition && afterCondition) {
+      backgroundRef.current.style.background = 'rgb(var(--color-price-higher-light) / 0.19)';
+    } else if (beforeCondition) {
+      backgroundRef.current.style.background =
+        'linear-gradient(90deg, rgb(var(--color-price-higher-light) / 0.19) 0%, rgb(var(--color-price-lower-light) / 0.19) 100%)';
+    } else if (afterCondition) {
+      backgroundRef.current.style.background =
+        'linear-gradient(90deg, rgb(var(--color-price-lower-light) / 0.19) 0%, rgb(var(--color-price-higher-light) / 0.19) 100%)';
+    } else {
+      backgroundRef.current.style.background = 'rgb(var(--color-price-lower-light) / 0.19)';
     }
   }, [beforeCondition, afterCondition]);
 
-  return { beforeCondition, toggleBeforeCondition, afterCondition, toggleAfterCondition };
+  return { beforeCondition, afterCondition, toggleConditions, onLoadBackgroundRef };
 }
 
 export default useBackgroundGradient;
