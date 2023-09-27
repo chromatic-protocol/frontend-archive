5229e3512399b78527afe0189799029bede34a91
austin-builds <austin.31@quarkonix.com>
2023-09-27 18:59:35 +0900

    feat: remove clp tokens from chromatic lp

---
+++ src/hooks/useRemoveChromaticLp.ts
@@ -0,0 +1,41 @@
+import { isNil } from 'ramda';
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+import { parseUnits } from 'viem';
+import { useAccount } from 'wagmi';
+import { useAppSelector } from '~/store';
+import { useChromaticClient } from './useChromaticClient';
+import { useMarket } from './useMarket';
+
+export const useRemoveChromaticLp = () => {
+  const { client, lpClient } = useChromaticClient();
+  const { currentMarket } = useMarket();
+  const { address } = useAccount();
+  const selectedLp = useAppSelector((state) => state.lp.selectedLp);
+  const [isRemovalPending, setIsRemovalPending] = useState(false);
+
+  const onRemoveChromaticLp = async (amount: string) => {
+    try {
+      if (isNil(address)) {
+        return;
+      }
+      if (isNil(selectedLp)) {
+        return;
+      }
+      setIsRemovalPending(true);
+      const parsedAmount = parseUnits(amount, selectedLp.decimals);
+      const lp = lpClient.lp();
+      const isClpApproved = await lp.approveLpTokenToLp(selectedLp.address, parsedAmount);
+      if (!isClpApproved) {
+        throw new Error('CLP approval failed.');
+      }
+      const removalResponse = await lp.removeLiquidity(selectedLp.address, parsedAmount);
+      toast('Removal completed.');
+      setIsRemovalPending(false);
+    } catch (error) {
+      setIsRemovalPending(false);
+    }
+  };
+
+  return { onRemoveChromaticLp, isRemovalPending };
+};
