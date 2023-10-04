fadc8c1a7316bd06d5b613228461ab8f227a8db7
austin-builds <austin.31@quarkonix.com>
2023-10-04 18:21:32 +0900

    Revert "fix: filter if receipt is in progress"
    This reverts commit 3b6035099a4dbf397b51fc613d2626825dac92ba.

--- src/stories/molecule/PoolProgressV2/hooks.tsx
+++ src/stories/molecule/PoolProgressV2/hooks.tsx
@@ -44,7 +44,6 @@ export const usePoolProgressV2 = () => {
   const { receipts } = useLpReceipts();
   const mintingReceipts = receipts?.filter((receipt) => receipt.action === 'minting');
   const burningReceipts = receipts?.filter((receipt) => receipt.action === 'burning');
-  const receiptsInProgress = receipts?.filter((receipt) => !receipt.isClosed);
 
   return {
     openButtonRef,
