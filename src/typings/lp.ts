5d3f26b093887a50a5dc9c295b29520d81b76bec
austin-builds <austin.31@quarkonix.com>
2023-10-06 22:46:30 +0900

    refactor: typings for lp

--- src/typings/lp.ts
+++ src/typings/lp.ts
@@ -18,3 +18,33 @@ export interface ChromaticLp {
   settlementToken: Token;
   market: Market;
 }
+
+export interface LpReceipt {
+  key: string;
+  id: bigint;
+  oracleVersion: bigint;
+  timestamp: bigint;
+  amount: bigint;
+  mintedAmount: bigint;
+  burnedAmount: bigint;
+  remainedAmount: bigint;
+  hasReturnedValue: boolean;
+  isIssued: boolean;
+  isSettled: boolean;
+  recipient: Address;
+  action: 'minting' | 'burning';
+  status: 'standby' | 'completed';
+  message: string;
+  detail: [string, string | undefined];
+  token: {
+    name?: string;
+    decimals?: number;
+    logo?: string;
+  };
+  blockNumber: bigint;
+}
+
+export type LpReceiptPartial = Pick<
+  LpReceipt,
+  'action' | 'isIssued' | 'isSettled' | 'id' | 'recipient'
+>;
