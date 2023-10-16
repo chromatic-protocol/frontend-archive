6caa1348f341d7f47b4834dcbe92ff87c0cbb670
austin-builds <austin.31@quarkonix.com>
2023-10-16 18:27:17 +0900

    wip: fetch lp receipts by apollo client

--- src/hooks/useLpReceipts.ts
+++ src/hooks/useLpReceipts.ts
@@ -1,11 +1,26 @@
+/**
+ * When encountering error `Cannot find module, or type declarations`, Run codegen with `yarn codegen`
+ * @austin-builds
+ */
+
+import { ApolloClient, useApolloClient } from '@apollo/client';
 import { iChromaticLpABI } from '@chromatic-protocol/liquidity-provider-sdk/contracts';
 import axios from 'axios';
 import { isNil, isNotNil } from 'ramda';
 import useSWRInfinite from 'swr/infinite';
 import { decodeEventLog } from 'viem';
 import { Address, useAccount } from 'wagmi';
+import {
+  AddLiquiditiesDocument,
+  AddLiquiditySettledsDocument,
+  AddLiquidity_OrderBy,
+  OrderDirection,
+  RemoveLiquiditiesDocument,
+  RemoveLiquiditySettledsDocument,
+  RemoveLiquidity_OrderBy,
+} from '~/__generated__/graphql';
 import { ARBISCAN_API_KEY, ARBISCAN_API_URL, BLOCK_CHUNK, PAGE_SIZE } from '~/constants/arbiscan';
-import { LpReceipt } from '~/typings/lp';
+import { LpReceipt, LpToken } from '~/typings/lp';
 import { ResponseLog } from '~/typings/position';
 import { checkAllProps } from '~/utils';
 import { divPreserved, formatDecimals } from '~/utils/number';
@@ -50,6 +65,7 @@ const getLpReceiptsByLog = async (params: GetLpReceiptByLogParams) => {
           topics: log.topics,
           strict: false,
         });
+        console.log(decoded, 'decoded');
         return {
           ...decoded,
           blockNumber: BigInt(log.blockNumber),
@@ -136,11 +152,11 @@ const getLpReceiptsByLog = async (params: GetLpReceiptByLogParams) => {
   return { receiptArray };
 };
 
-type UseLpReceiptsNext = {
+type UseLpReceipts = {
   action: 'all' | 'minting' | 'burning';
 };
 
-export const useLpReceipts = (props: UseLpReceiptsNext) => {
+export const useLpReceipts = (props: UseLpReceipts) => {
   const { isReady, lpClient, client } = useChromaticClient();
   const { address } = useAccount();
   const { currentMarket } = useMarket();
@@ -345,4 +361,262 @@ export const useLpReceipts = (props: UseLpReceiptsNext) => {
     receiptsData,
     onFetchNextLpReceipts,
   };
+};
+
+type GetReceiptsArgs = {
+  walletAddress: Address;
+  lpAddress: Address;
+  toBlockTimestamp?: bigint;
+};
+
+const getAddReceipts = async (client: ApolloClient<object>, args: GetReceiptsArgs) => {
+  const { walletAddress, lpAddress, toBlockTimestamp } = args;
+  const {
+    data: { addLiquidities },
+  } = await client.query({
+    query: AddLiquiditiesDocument,
+    variables: {
+      count: 1,
+      orderBy: AddLiquidity_OrderBy.BlockTimestamp,
+      orderDirection: OrderDirection.Desc,
+      walletAddress,
+      lpAddress,
+      toBlockTimestamp,
+    },
+  });
+  const fromId = addLiquidities[0].receiptId;
+  const endId = addLiquidities[addLiquidities.length - 1].receiptId;
+  const {
+    data: { addLiquiditySettleds },
+  } = await client.query({
+    query: AddLiquiditySettledsDocument,
+    variables: { fromId, endId, lpAddress },
+  });
+
+  const addSettledMap = addLiquiditySettleds.reduce((map, current) => {
+    const { lpTokenAmount, receiptId } = current;
+    map.set(receiptId, {
+      id: receiptId,
+      mintedAmount: lpTokenAmount,
+      isSettled: true,
+    } as LpReceipt);
+    return map;
+  }, new Map<bigint, LpReceipt>());
+  const addMap = addLiquidities.reduce((map, current) => {
+    const { receiptId, recipient, amount, oracleVersion, blockNumber, blockTimestamp } = current;
+    const settled = addSettledMap.get(receiptId) ?? { isSettled: false };
+    map.set(receiptId, {
+      action: 'minting',
+      id: receiptId,
+      lpAddress,
+      recipient,
+      amount,
+      oracleVersion,
+      blockNumber,
+      blockTimestamp,
+      isIssued: true,
+      ...settled,
+    } as LpReceipt);
+    return map;
+  }, new Map<bigint, LpReceipt>());
+  return addMap;
+};
+
+const getRemoveReceipts = async (client: ApolloClient<object>, args: GetReceiptsArgs) => {
+  const { walletAddress, lpAddress, toBlockTimestamp } = args;
+  const {
+    data: { removeLiquidities },
+  } = await client.query({
+    query: RemoveLiquiditiesDocument,
+    variables: {
+      count: 1,
+      orderBy: RemoveLiquidity_OrderBy.BlockTimestamp,
+      orderDirection: OrderDirection.Desc,
+      walletAddress,
+      lpAddress,
+      toBlockTimestamp,
+    },
+  });
+  const fromId = removeLiquidities[0].receiptId;
+  const endId = removeLiquidities[removeLiquidities.length - 1].receiptId;
+  const {
+    data: { removeLiquiditySettleds },
+  } = await client.query({
+    query: RemoveLiquiditySettledsDocument,
+    variables: {
+      fromId,
+      endId,
+      lpAddress,
+    },
+  });
+  const removeSettledMap = removeLiquiditySettleds.reduce((map, current) => {
+    const { receiptId, witdrawnSettlementAmount, refundedAmount } = current;
+    map.set(receiptId, {
+      id: receiptId,
+      burnedAmount: witdrawnSettlementAmount,
+      remainedAmount: refundedAmount,
+      isSettled: true,
+      hasReturnedValue: refundedAmount !== 0n,
+    } as LpReceipt);
+    return map;
+  }, new Map<bigint, LpReceipt>());
+  const removeMap = removeLiquidities.reduce((map, current) => {
+    const { receiptId, recipient, lpTokenAmount, oracleVersion, blockNumber, blockTimestamp } =
+      current;
+    const settled = removeSettledMap.get(receiptId) ?? { isSettled: false };
+    map.set(receiptId, {
+      action: 'burning',
+      id: receiptId,
+      lpAddress,
+      recipient,
+      amount: lpTokenAmount,
+      oracleVersion,
+      blockNumber,
+      blockTimestamp,
+      isIssued: true,
+      ...settled,
+    } as LpReceipt);
+    return map;
+  }, new Map<bigint, LpReceipt>());
+  return removeMap;
+};
+
+export const useLpReceiptsNext = (props: UseLpReceipts) => {
+  const { isReady, lpClient, client } = useChromaticClient();
+  const apolloClient = useApolloClient();
+  const { address } = useAccount();
+  const { currentMarket } = useMarket();
+  const { tokens } = useSettlementToken();
+  const { action } = props;
+
+  useSWRInfinite(
+    (pageIndex, previousData) => {
+      if (!isReady || isNil(apolloClient)) {
+        return null;
+      }
+      if (previousData && !previousData.receipts) {
+        return null;
+      }
+      const fetchKey = {
+        key: 'getChromaticLpReceipts',
+        walletAddress: address,
+        currentMarket,
+        tokens,
+        action,
+        pageIndex,
+      };
+      if (!checkAllProps(fetchKey)) {
+        return null;
+      }
+      return {
+        ...fetchKey,
+        toBlockTimestamp: previousData?.toBlockTimestamp as bigint | undefined,
+      };
+    },
+    async ({ walletAddress, currentMarket, tokens, action, toBlockTimestamp }) => {
+      const registry = lpClient.registry();
+      const lp = lpClient.lp();
+      const lpAddresses = await registry.lpListByMarket(currentMarket.address);
+      const clpTokensPromise = await PromiseOnlySuccess(
+        lpAddresses.map(async (lpAddress) => {
+          const tokenMeta = await lp.lpTokenMeta(lpAddress);
+          return {
+            address: lpAddress,
+            ...tokenMeta,
+          };
+        })
+      );
+      const clpTokens = clpTokensPromise.reduce((record, lpToken) => {
+        record[lpToken.address] = lpToken;
+        return record;
+      }, {} as Record<Address, LpToken>);
+      const settlementToken = tokens.find((token) => token.address === currentMarket.tokenAddress);
+      let receipts: LpReceipt[] = [];
+      for (let index = 0; index < lpAddresses.length; index++) {
+        const lpAddress = lpAddresses[index];
+
+        const addMap = await getAddReceipts(apolloClient, {
+          walletAddress,
+          lpAddress,
+          toBlockTimestamp,
+        });
+        const removeMap = await getRemoveReceipts(apolloClient, {
+          walletAddress,
+          lpAddress,
+          toBlockTimestamp,
+        });
+        const currentReceipts = Array.from(addMap.values()).concat(...removeMap.values());
+        receipts = receipts.concat(currentReceipts);
+      }
+      receipts.sort((previous, next) => {
+        if (isNil(previous.blockTimestamp) && isNil(next.blockTimestamp)) {
+          return 0;
+        }
+        if (isNil(previous.blockTimestamp) && isNotNil(next.blockTimestamp)) {
+          return 1;
+        }
+        if (isNil(previous.blockTimestamp) && isNil(next.blockTimestamp)) {
+          return -1;
+        }
+        return previous.blockTimestamp < next.blockTimestamp ? 1 : -1;
+      });
+      receipts = receipts.slice(0, 1);
+      const detailedReceipts = receipts.map(async (receipt) => {
+        const clpToken = clpTokens[receipt.lpAddress];
+        const status = receipt.isIssued && receipt.isSettled ? 'completed' : 'standby';
+        let detail = '';
+        const token = {
+          name: receipt.action === 'minting' ? clpToken?.symbol : settlementToken?.name,
+          decimals: receipt.action === 'minting' ? clpToken.decimals : settlementToken?.decimals,
+          logo: receipt.action === 'burning' ? settlementToken?.image : undefined,
+        };
+
+        if (status === 'completed' && receipt.action === 'minting' && receipt.isSettled) {
+          detail = formatDecimals(receipt.mintedAmount, token.decimals, 2, true) + ' ' + token.name;
+        }
+
+        if (status === 'completed' && receipt.action === 'burning' && receipt.isSettled) {
+          detail = formatDecimals(receipt.burnedAmount, token.decimals, 2, true) + ' ' + token.name;
+        }
+        let message = status === 'standby' ? 'Waiting for the next oracle round' : 'Completed';
+        const key = `${action}-receipt-${receipt.id}-${receipt.action}-${status}`;
+
+        if (receipt.action === 'burning' && receipt.remainedAmount > 0n) {
+          const dividedByAmount = divPreserved(
+            receipt.remainedAmount,
+            receipt.amount,
+            clpToken.decimals
+          );
+          const returnedRatio = formatDecimals(dividedByAmount * 100n, clpToken.decimals, 2, true);
+          message = `${returnedRatio}% withdrawn`;
+        }
+        const remainedDetail = isNotNil(receipt.remainedAmount)
+          ? formatDecimals(receipt.remainedAmount, clpToken.decimals, 2, true)
+          : undefined;
+
+        const oracleProvider = await client
+          .market()
+          .contracts()
+          .oracleProvider(currentMarket.address);
+        const oracleValue = await oracleProvider.read.atVersion([receipt.oracleVersion]);
+        const { timestamp } = oracleValue;
+
+        return {
+          ...receipt,
+          key,
+          status,
+          message,
+          detail: [detail, remainedDetail],
+
+          timestamp,
+          token,
+        } satisfies LpReceipt;
+      });
+
+      return {
+        receipts: PromiseOnlySuccess(detailedReceipts),
+        toBlockTimestamp: receipts[receipts.length - 1].blockTimestamp,
+      };
+    }
+  );
 };
