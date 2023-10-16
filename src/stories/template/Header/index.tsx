75831dd053c917c376f849cfa883b1a259106009
dia.nn <dia.nn@quarkonix.com>
2023-10-16 16:00:42 +0900

    feat: add menu, refactor Header

--- src/stories/template/Header/index.tsx
+++ src/stories/template/Header/index.tsx
@@ -11,6 +11,14 @@ import { useHeader } from './hooks';
 export function Header() {
   const { isActiveLink, walletPopoverProps } = useHeader();
 
+  const links = [
+    { to: 'trade', text: 'Trade', className: '' },
+    { to: 'pool', text: 'Pools', className: '' },
+    { to: 'trade3', text: 'Trade3', className: 'text-primary-light' },
+    { to: 'pool3', text: 'Pools3', className: 'text-primary-light' },
+    { to: 'airdrop', text: 'Airdrop', className: '' },
+  ];
+
   return (
     <header className="Header">
       <div className="h-[70px] px-10 py-5 flex items-center justify-between">
@@ -18,35 +26,17 @@ export function Header() {
           <Link to="/" className="mr-4 font-bold" title="Chromatic">
             <LogoSimple className="text-primary h-9" />
           </Link>
-          <Link
-            to="/trade"
-            className={`link ${isActiveLink('trade') ? '!border-primary' : '!border-transparent'}`}
-          >
-            Trade
-          </Link>
-          <Link
-            to="/pool"
-            className={`link ${isActiveLink('pool') ? '!border-primary' : '!border-transparent'}`}
-          >
-            Pools
-          </Link>
-
-          <Link
-            to="/trade3"
-            className={`link text-primary-light ${
-              isActiveLink('trade3') ? '!border-primary-light' : '!border-transparent'
-            }`}
-          >
-            Trade3
-          </Link>
-          <Link
-            to="/pool3"
-            className={`link text-primary-light ${
-              isActiveLink('pool3') ? '!border-primary-light' : '!border-transparent'
-            }`}
-          >
-            Pools3
-          </Link>
+          {links.map((link) => (
+            <Link
+              key={link.to}
+              to={`/${link.to}`}
+              className={`link ${link.className} ${
+                isActiveLink(link.to) ? '!border-primary' : '!border-transparent'
+              }`}
+            >
+              {link.text}
+            </Link>
+          ))}
         </div>
         <div className="flex">
           <div className="hidden mr-4">
