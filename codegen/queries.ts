615082c2526baa82a5f7f97d3a7d3a1aa5e6e516
austin-builds <austin.31@quarkonix.com>
2023-10-17 18:25:03 +0900

    refactor: queries for lp receipts and counts

--- codegen/queries.ts
+++ codegen/queries.ts
@@ -1,86 +1,139 @@
-import { gql } from '@apollo/client';
+import { gql } from 'graphql-request';
 
-export const ADD_LIQUIDITIES = gql(`
-query AddLiquidities(
-  $count: Int!
-  $orderBy: AddLiquidity_orderBy!
-  $orderDirection: OrderDirection!
-  $walletAddress: Bytes!
-  $lpAddress: Bytes!
-  $toBlockTimestamp: BigInt
-) {
-  addLiquidities(
-    orderDirection: $orderDirection
-    orderBy: $orderBy
-    where: { lp: $lpAddress, recipient: $walletAddress, blockTimestamp_lt: $toBlockTimestamp }
-    first: $count
+export const ADD_LIQUIDITIES = gql`
+  query AddLiquidities(
+    $count: Int!
+    $orderBy: AddLiquidity_orderBy!
+    $orderDirection: OrderDirection!
+    $walletAddress: Bytes!
+    $lpAddress: Bytes!
+    $toBlockTimestamp: BigInt
   ) {
-    id
-    lp
-    receiptId
-    recipient
-    oracleVersion
-    amount
-    blockNumber
-    blockTimestamp
-    transactionHash
+    addLiquidities(
+      orderDirection: $orderDirection
+      orderBy: $orderBy
+      where: { lp: $lpAddress, recipient: $walletAddress, blockTimestamp_lt: $toBlockTimestamp }
+      first: $count
+    ) {
+      id
+      lp
+      receiptId
+      provider
+      recipient
+      oracleVersion
+      amount
+      blockNumber
+      blockTimestamp
+      transactionHash
+    }
   }
-}`);
+`;
 
-export const ADD_LIQUIDITY_SETTLEDS = gql(`
-query AddLiquiditySettleds($fromId: BigInt!, $endId: BigInt!, $lpAddress: Bytes!) {
-  addLiquiditySettleds(where: { receiptId_gt: $fromId, receiptId_lt: $endId, lp: $lpAddress }) {
-    id
-    lp
-    receiptId
-    settlementAdded
-    lpTokenAmount
-    blockNumber
-    blockTimestamp
-    transactionHash
+export const ADD_LIQUIDITY_SETTLEDS = gql`
+  query AddLiquiditySettleds($fromId: BigInt!, $endId: BigInt!, $lpAddress: Bytes!) {
+    addLiquiditySettleds(where: { receiptId_gte: $fromId, receiptId_lte: $endId, lp: $lpAddress }) {
+      id
+      lp
+      receiptId
+      settlementAdded
+      lpTokenAmount
+      blockNumber
+      blockTimestamp
+      transactionHash
+    }
   }
-}
-`);
+`;
 
-export const REMOVE_LIQUIDITIES = gql(`
-query RemoveLiquidities(
-  $count: Int!
-  $orderBy: RemoveLiquidity_orderBy!
-  $orderDirection: OrderDirection!
-  $walletAddress: Bytes!
-  $lpAddress: Bytes!
-  $toBlockTimestamp: BigInt
-) {
-  removeLiquidities(
-    orderDirection: $orderDirection
-    orderBy: $orderBy
-    where: { lp: $lpAddress, recipient: $walletAddress, blockTimestamp_lt: $toBlockTimestamp }
-    first: $count
+export const REMOVE_LIQUIDITIES = gql`
+  query RemoveLiquidities(
+    $count: Int!
+    $orderBy: RemoveLiquidity_orderBy!
+    $orderDirection: OrderDirection!
+    $walletAddress: Bytes!
+    $lpAddress: Bytes!
+    $toBlockTimestamp: BigInt
   ) {
-    id
-    lp
-    receiptId
-    recipient
-    oracleVersion
-    lpTokenAmount
-    blockNumber
-    blockTimestamp
-    transactionHash
+    removeLiquidities(
+      orderDirection: $orderDirection
+      orderBy: $orderBy
+      where: { lp: $lpAddress, recipient: $walletAddress, blockTimestamp_lt: $toBlockTimestamp }
+      first: $count
+    ) {
+      id
+      lp
+      receiptId
+      recipient
+      oracleVersion
+      lpTokenAmount
+      blockNumber
+      blockTimestamp
+      transactionHash
+    }
   }
-}`);
+`;
 
-export const REMOVE_LIQUIDITY_SETTLEDS = gql(`
-query RemoveLiquiditySettleds($fromId: BigInt!, $endId: BigInt!, $lpAddress: Bytes!) {
-  removeLiquiditySettleds(where: { receiptId_gt: $fromId, receiptId_lt: $endId, lp: $lpAddress }) {
-    id
-    lp
-    receiptId
-    burningAmount
-    witdrawnSettlementAmount
-    refundedAmount
-    blockNumber
-    blockTimestamp
-    transactionHash
+export const REMOVE_LIQUIDITY_SETTLEDS = gql`
+  query RemoveLiquiditySettleds($fromId: BigInt!, $endId: BigInt!, $lpAddress: Bytes!) {
+    removeLiquiditySettleds(
+      where: { receiptId_gte: $fromId, receiptId_lte: $endId, lp: $lpAddress }
+    ) {
+      id
+      lp
+      receiptId
+      burningAmount
+      witdrawnSettlementAmount
+      refundedAmount
+      blockNumber
+      blockTimestamp
+      transactionHash
+    }
   }
-}
-`);
+`;
+
+export const ADD_LIQUIDITIY_COUNT = gql`
+  query AddLiquidityCount($walletAddress: Bytes!, $lpAddress: Bytes!) {
+    addLiquidities(where: { lp: $lpAddress, recipient: $walletAddress }) {
+      id
+      lp
+      receiptId
+      recipient
+      provider
+    }
+  }
+`;
+
+export const ADD_LIQUIDITY_SETTLED_COUNT = gql`
+  query AddLiquiditySettledCount($lpAddress: Bytes!) {
+    addLiquiditySettleds(where: { lp: $lpAddress }) {
+      id
+      lp
+      receiptId
+      recipient
+      provider
+    }
+  }
+`;
+
+export const REMOVE_LIQUIDITY_COUNT = gql`
+  query RemoveLiquidityCount($walletAddress: Bytes!, $lpAddress: Bytes!) {
+    removeLiquidities(where: { lp: $lpAddress, recipient: $walletAddress }) {
+      id
+      lp
+      receiptId
+      recipient
+      provider
+    }
+  }
+`;
+
+export const REMOVE_LIQUIDITY_SETTLED_COUNT = gql`
+  query RemoveLiquiditySettledCount($lpAddress: Bytes!) {
+    removeLiquiditySettleds(where: { lp: $lpAddress }) {
+      id
+      lp
+      receiptId
+      recipient
+      provider
+    }
+  }
+`;
