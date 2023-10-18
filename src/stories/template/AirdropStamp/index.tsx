ad71ca68a73ba16d3e405a558ed8719f9678fc3f
dia.nn <dia.nn@quarkonix.com>
2023-10-18 14:12:01 +0900

    feat: AirdropStamp detail

--- src/stories/template/AirdropStamp/index.tsx
+++ src/stories/template/AirdropStamp/index.tsx
@@ -11,7 +11,7 @@ export const AirdropStamp = (props: AirdropStampProps) => {
   const week = [
     { name: 'mon', status: 'success', point: '10' },
     { name: 'tue', status: 'fail', point: '10' },
-    { name: 'wed', status: 'today', point: '10' },
+    { name: 'wed', status: 'success', point: '10' },
     { name: 'thu', status: 'empty', point: '10' },
     { name: 'fri', status: 'empty', point: '10' },
     { name: 'sat', status: 'empty', point: '10' },
@@ -21,7 +21,7 @@ export const AirdropStamp = (props: AirdropStampProps) => {
   return (
     <div className="p-5 text-left panel AirdropStamp">
       <div className="flex pb-5 border-b">
-        <div className="w-1/2 pr-10">
+        <div className="w-1/2 pl-5 pr-10">
           <h4 className="mb-5 text-3xl text-primary-light">Sign-In Rewards</h4>
           <div className="flex items-center gap-2 mb-4">
             <BoosterIcon className="w-6" />
@@ -32,7 +32,7 @@ export const AirdropStamp = (props: AirdropStampProps) => {
             <p className="text-xl">Sign-In 5 days in a week & get 50 extra credits</p>
           </div>
         </div>
-        <div className="flex flex-col justify-between w-1/2 pl-10 border-l">
+        <div className="flex flex-col justify-between w-1/2 pl-10 pr-5 border-l">
           <div className="flex">
             <h4 className="mb-5 text-3xl text-primary-light">Currently Claimable</h4>
             <Button label="Claim Now" size="sm" css="active" className="!text-base ml-auto" />
@@ -50,11 +50,21 @@ export const AirdropStamp = (props: AirdropStampProps) => {
           </div>
         </div>
       </div>
-      <div className="flex justify-between mt-6">
+      <div className="flex justify-between px-10 mt-6">
         {week.map((item) => (
           <div key={item.name} className={`stamp stamp-${item.status}`}>
             <h5 className="text-xl capitalize text-primary-light">{item.name}</h5>
-            <Avatar className="w-20 h-20 mt-3" />
+            <Avatar
+              className="w-20 h-20 mt-3"
+              src={
+                item.status === 'success'
+                  ? '/src/assets/images/stamp_success.png'
+                  : item.status === 'fail'
+                  ? '/src/assets/images/stamp_fail.png'
+                  : ''
+              }
+            />
+
             {item.status === 'success' && <p className="mt-2 text-xl">+{item.point}</p>}
           </div>
         ))}
