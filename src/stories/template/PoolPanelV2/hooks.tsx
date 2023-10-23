f26f7f7c5d742596b9e3f46c4e02da97b354abed
austin-builds <austin.31@quarkonix.com>
2023-10-23 14:34:45 +0900

    fix: switch token by changing tabs

--- src/stories/template/PoolPanelV2/hooks.tsx
+++ src/stories/template/PoolPanelV2/hooks.tsx
@@ -1,24 +1,19 @@
 import { isNil, isNotNil } from 'ramda';
-import { useCallback, useMemo, useState } from 'react';
-import { toast } from 'react-toastify';
-import { formatUnits, parseUnits } from 'viem';
-import { usePublicClient } from 'wagmi';
+import { useMemo, useState } from 'react';
+import { parseUnits } from 'viem';
 import { useAddChromaticLp } from '~/hooks/useAddChromaticLp';
 import { useLiquidityPool } from '~/hooks/useLiquidityPool';
-import { useOwnedLiquidityPools } from '~/hooks/useOwnedLiquidityPools';
 import usePoolInput from '~/hooks/usePoolInput';
 import { useRemoveChromaticLp } from '~/hooks/useRemoveChromaticLp';
 import { useSettlementToken } from '~/hooks/useSettlementToken';
 import { useTokenBalances } from '~/hooks/useTokenBalance';
-import { useAppDispatch, useAppSelector } from '~/store';
-import { poolsAction } from '~/store/reducer/pools';
-import { divPreserved, formatDecimals, withComma } from '~/utils/number';
+import { useAppSelector } from '~/store';
+import { formatDecimals, withComma } from '~/utils/number';
 
 export function usePoolPanelV2() {
   const { currentToken, isTokenLoading } = useSettlementToken();
   const { rangeChartRef } = usePoolInput();
   const { tokenBalances, isTokenBalanceLoading } = useTokenBalances();
-  const { currentOwnedPool } = useOwnedLiquidityPools();
   const {
     liquidity: {
       longTotalMaxLiquidity,
@@ -27,37 +22,13 @@ export function usePoolPanelV2() {
       shortTotalUnusedLiquidity,
     },
   } = useLiquidityPool();
-  const selectedBins = useAppSelector((state) => state.pools.selectedBins);
-  const isRemoveModalOpen = useAppSelector((state) => state.pools.isModalOpen);
   const selectedLp = useAppSelector((state) => state.lp.selectedLp);
-
-  const direction = useAppSelector((state) => state.pools.selectedDirection);
-  const dispatch = useAppDispatch();
-  const binDecimals =
-    isNotNil(currentOwnedPool) && currentOwnedPool.bins.length > 0
-      ? currentOwnedPool.bins[0].clbTokenDecimals
-      : 1;
-
-  const ownedLongLiquidityBins = useMemo(
-    () =>
-      currentOwnedPool?.bins.filter((bin) => bin.clbTokenBalance > 0n && bin.baseFeeRate > 0n) ||
-      [],
-    [currentOwnedPool]
-  );
-  const ownedShortLiquidityBins = useMemo(
-    () =>
-      currentOwnedPool?.bins.filter((bin) => bin.clbTokenBalance > 0n && bin.baseFeeRate < 0n) ||
-      [],
-    [currentOwnedPool]
-  );
-
+  const [selectedTab, setSelectedTab] = useState(0);
+  const onTabChange = (tabIndex: number) => {
+    setSelectedTab(tabIndex);
+    return;
+  };
   const [isBinValueVisible, setIsBinValueVisible] = useState(false);
-
-  const settlementTokenBalance = useMemo(() => {
-    if (tokenBalances && currentToken && tokenBalances[currentToken.address])
-      return formatDecimals(tokenBalances[currentToken.address], currentToken.decimals, 0);
-    return '-';
-  }, [tokenBalances, currentToken]);
   const liquidityFormatter = Intl.NumberFormat('en', {
     useGrouping: false,
     notation: 'compact',
@@ -66,52 +37,23 @@ export function usePoolPanelV2() {
     minimumFractionDigits: 0,
   });
   const isAssetsLoading = isTokenLoading || isTokenBalanceLoading;
-  const onSelectAllClick = useCallback(
-    (selectedIndex: number) => {
-      switch (selectedIndex) {
-        case 1: {
-          if (
-            direction === 'long' &&
-            selectedBins.filter((bin) => bin.baseFeeRate > 0).length ===
-              ownedLongLiquidityBins.length
-          ) {
-            dispatch(poolsAction.onBinsReset());
-          } else {
-            dispatch(poolsAction.onAllBinsSelect(ownedLongLiquidityBins));
-          }
-          break;
-        }
-        case 0: {
-          if (
-            direction === 'short' &&
-            selectedBins.filter((bin) => bin.baseFeeRate < 0).length ===
-              ownedShortLiquidityBins.length
-          ) {
-            dispatch(poolsAction.onBinsReset());
-          } else {
-            dispatch(poolsAction.onAllBinsSelect(ownedShortLiquidityBins));
-          }
-          break;
-        }
-        default: {
-          toast('Invalid access');
-        }
-      }
-    },
-    [ownedLongLiquidityBins, ownedShortLiquidityBins, selectedBins, direction]
-  );
-
-  const binLength = currentOwnedPool?.bins.length || 0;
 
   const tokenName = currentToken?.name || '-';
   const tokenImage = currentToken?.image;
   const clpImage = selectedLp?.image;
 
   // ----------------------------------------------------------------
-
-  const walletBalance = withComma(settlementTokenBalance);
-
-  const maxAmount = settlementTokenBalance;
+  const selectedBalance = useMemo(() => {
+    if (tokenBalances && currentToken && tokenBalances[currentToken.address]) {
+      return tokenBalances[currentToken.address];
+    }
+    if (isNotNil(selectedLp)) {
+      return selectedLp.balance;
+    }
+  }, [tokenBalances, currentToken, selectedLp]);
+  const selectedDecimals = selectedTab === 0 ? currentToken?.decimals : selectedLp?.decimals;
+  const maxAmount = formatDecimals(selectedBalance, selectedDecimals, selectedDecimals);
+  const formattedBalance = withComma(formatDecimals(selectedBalance, selectedDecimals, 2));
 
   const shortUsedLp = liquidityFormatter.format(
     +formatDecimals(shortTotalMaxLiquidity - shortTotalUnusedLiquidity, currentToken?.decimals)
@@ -125,85 +67,17 @@ export function usePoolPanelV2() {
   const longMaxLp = liquidityFormatter.format(
     +formatDecimals(longTotalMaxLiquidity, currentToken?.decimals)
   );
-  const bigTotalLiquidityValue =
-    currentOwnedPool?.bins.reduce((sum, current) => {
-      sum = sum + current.clbBalanceOfSettlement;
-      return sum;
-    }, 0n) ?? 0n;
-  const totalLiquidityValue = formatDecimals(bigTotalLiquidityValue, currentToken?.decimals, 2);
-
-  const bigTotalLiquidity =
-    currentOwnedPool?.bins.reduce((sum, current) => {
-      sum += current.liquidity;
-      return sum;
-    }, 0n) ?? 0n;
-  const bigTotalFreeLiquidity =
-    currentOwnedPool?.bins.reduce((sum, current) => {
-      if (current.clbBalanceOfSettlement > current.freeLiquidity) {
-        sum += current.freeLiquidity;
-      } else {
-        sum += current.clbBalanceOfSettlement;
-      }
-      return sum;
-    }, 0n) ?? 0n;
-  const totalFreeLiquidity = formatDecimals(bigTotalFreeLiquidity, currentToken?.decimals, 2);
-
-  const averageRemovableRate = currentToken
-    ? Number(
-        formatUnits(
-          divPreserved(
-            bigTotalFreeLiquidity * 100n,
-            bigTotalLiquidity === 0n ? 1n : bigTotalLiquidity,
-            binDecimals
-          ),
-          currentToken.decimals
-        )
-      ).toFixed(2)
-    : '-';
-
-  const onTabChange = (index: number) => {
-    dispatch(poolsAction.onDirectionToggle(index === 0 ? 'short' : 'long'));
-  };
-
-  const onRemoveSelectedClick = () => {
-    if (selectedBins.length > 0) {
-      dispatch(poolsAction.onModalOpen());
-    }
-  };
-  const isRemoveSelectedDisabled = selectedBins.length === 0;
-
-  const publicClient = usePublicClient();
-  const getTokenExplorer = useCallback(
-    (address: string) => {
-      try {
-        const rawUrl = publicClient.chain.blockExplorers?.default?.url;
-        if (isNil(rawUrl)) return;
-        const origin = new URL(rawUrl).origin;
-        if (isNil(origin) || isNil(address)) return;
-        return `${origin}/token/${address}`;
-      } catch (error) {
-        return;
-      }
-    },
-    [publicClient]
-  );
-
-  const isShortLiquidityBinsEmpty = ownedShortLiquidityBins.length === 0;
-  const isOwnedLongLiquidityBinsEmpty = ownedLongLiquidityBins?.length === 0;
-  const isSingleRemoveModalOpen = selectedBins.length === 1 && isRemoveModalOpen;
-  const isMultipleRemoveModalOpen = selectedBins.length > 1 && isRemoveModalOpen;
 
   const [amount, setAmount] = useState('');
   const isExceeded = useMemo(() => {
-    if (isNil(tokenBalances) || isNil(currentToken)) {
-      return;
+    if (isNil(selectedBalance) || isNil(selectedDecimals)) {
+      return true;
     }
-    const balance = tokenBalances?.[currentToken.address];
-    if (parseUnits(amount.toString(), currentToken.decimals) >= balance) {
+    if (parseUnits(amount.toString(), selectedDecimals) >= selectedBalance) {
       return true;
     }
     return false;
-  }, [amount, tokenBalances, currentToken]);
+  }, [amount, selectedBalance, selectedDecimals]);
   const { onAddChromaticLp, isAddPending } = useAddChromaticLp();
   const { onRemoveChromaticLp, isRemovalPending } = useRemoveChromaticLp();
   const onAmountChange = (value: string) => {
@@ -225,20 +99,22 @@ export function usePoolPanelV2() {
   }, [selectedLp]);
 
   return {
-    maxAmount,
-    setIsBinValueVisible,
+    selectedTab,
+    onTabChange,
     rangeChartRef,
+    setIsBinValueVisible,
 
     shortUsedLp,
     shortMaxLp,
     longUsedLp,
     longMaxLp,
-
     isBinValueVisible,
 
     isAssetsLoading,
-    amount,
     isExceeded,
+    amount,
+    maxAmount,
+    formattedBalance,
     formattedClp,
     isAddPending,
     isRemovalPending,
@@ -249,20 +125,5 @@ export function usePoolPanelV2() {
     tokenName,
     tokenImage,
     clpImage,
-    walletBalance,
-    totalLiquidityValue,
-    binLength,
-    totalFreeLiquidity,
-    averageRemovableRate,
-
-    onTabChange,
-    onSelectAllClick,
-    onRemoveSelectedClick,
-    isRemoveSelectedDisabled,
-    isShortLiquidityBinsEmpty,
-    isOwnedLongLiquidityBinsEmpty,
-
-    isSingleRemoveModalOpen,
-    isMultipleRemoveModalOpen,
   };
 }
