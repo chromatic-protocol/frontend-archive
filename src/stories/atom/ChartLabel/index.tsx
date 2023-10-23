2498aed55a1febc0c6889a997f4d528221f95d55
dia.nn <dia.nn@quarkonix.com>
2023-10-23 15:41:46 +0900

    fix: ChartLabel color, prop

--- src/stories/atom/ChartLabel/index.tsx
+++ src/stories/atom/ChartLabel/index.tsx
@@ -14,11 +14,11 @@ export const ChartLabel = (props: ChartLabelProps) => {
   return (
     <div className={`flex items-center gap-[6px] ChartLabel ${className}`}>
       <span
-        className={`inline-block w-3 h-3 bg-gradient-to-b from-${color} to-${color}/10 ${
-          translucent ? 'opacity-30' : 0
+        className={`inline-flex w-3 h-3 bg-gradient-to-b from-primary to-primary/10 !from-${color} !to-${color}/10 ${thumbClass} ${
+          translucent ? 'opacity-30' : ''
         }`}
       />
-      <p className="text-sm leading-none text-primary-light">{label}</p>
+      <p className="text-sm leading-none capitalize text-primary-light">{label}</p>
     </div>
   );
 };
