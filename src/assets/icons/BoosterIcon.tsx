f58b6ef633354f1415c2111815a8cf96b214d28a
dia.nn <dia.nn@quarkonix.com>
2023-10-24 18:37:49 +0900

    fix: props naming convention

--- src/assets/icons/BoosterIcon.tsx
+++ src/assets/icons/BoosterIcon.tsx
@@ -3,9 +3,9 @@ export default function BoosterIcon(props: any) {
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
       <path
         className="fill-current"
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M12 20.667a8.667 8.667 0 1 0 0-17.334 8.667 8.667 0 0 0 0 17.334Zm0 2c5.89 0 10.666-4.776 10.666-10.667S17.891 1.333 12 1.333 1.333 6.11 1.333 12 6.109 22.667 12 22.667Z"
-        clip-rule="evenodd"
+        clipRule="evenodd"
       />
       <path
         className="fill-current"
