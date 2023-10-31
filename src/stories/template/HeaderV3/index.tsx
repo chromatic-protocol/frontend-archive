d52082039ce3ea80311f5155428e5461f2325251
dia.nn <dia.nn@quarkonix.com>
2023-10-31 18:24:52 +0900

    feat: update HeaderV3 menu - hide unused (v1)

--- src/stories/template/HeaderV3/index.tsx
+++ src/stories/template/HeaderV3/index.tsx
@@ -17,10 +17,8 @@ export const HeaderV3 = (props: HeaderV3Props) => {
   const { isActiveLink, walletPopoverProps } = useHeaderV3();
 
   const links = [
-    { to: 'trade', text: 'Trade', className: '' },
-    { to: 'pool', text: 'Pools', className: '' },
-    { to: 'trade3', text: 'Trade3', className: 'text-primary-light' },
-    { to: 'pool3', text: 'Pools3', className: 'text-primary-light' },
+    { to: 'trade3', text: 'Trade', className: '' },
+    { to: 'pool3', text: 'Pools', className: '' },
     { to: 'airdrop', text: 'Airdrop', className: '' },
   ];
 
