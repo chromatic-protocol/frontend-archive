a8f74d900c657358c2198ef4ab8a50ab5ed5a354
jaycho-46 <jay.46@quarkonix.com>
2023-11-02 11:10:53 +0900

    fix: restore lp.ts

---
+++ src/configs/lp.ts
@@ -0,0 +1,5 @@
+export const LP_TAG_ORDER: Record<string, number> = {
+  'Low Risk': 0,
+  'Mid Risk': 1,
+  'High Risk': 2,
+};
--- src/configs/subgraph.ts
+++ src/configs/subgraph.ts
@@ -4,9 +4,3 @@ export const HASURA_API_URL = 'https://hasura-arbitrum-goerli.api.chromatic.fina
 
 export const SUBGRAPH_API_WS_URL =
   'wss://graph-ws-arbitrum-goerli.api.chromatic.finance/subgraphs/name';
-
-export const LP_TAG_ORDER: Record<string, number> = {
-  'Low Risk': 0,
-  'Mid Risk': 1,
-  'High Risk': 2,
-};
